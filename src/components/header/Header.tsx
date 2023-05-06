2571625fc94ef5344883aea45840554c3aad8872
Gina Park <koyrkr@gmail.com>
2023-05-06 15:37:28 +0900

    fix: fix Header layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,19 +4,21 @@ import { HeaderWrapper, Logo, Menu, Noti } from './Header.styled';
 function Header() {
   return (
     <HeaderWrapper>
-      <Menu>menu</Menu>
-      {/* 예제 코드 */}
-      <Layout.FlexCol>
-        <Font.Body type="12_regular" color="SYSTEM_ERROR" ml={2}>
-          123
-        </Font.Body>
-        <Font.Display type="24_bold">123</Font.Display>
-      </Layout.FlexCol>
-      <Layout.FlexRow>
-        <Button.Large type="white_fill" text="버튼" status="normal" />
+      <Layout.FlexRow justifyContent="space-between" w="100%" alignItems="center">
+        <Menu>menu</Menu>
+        {/* 예제 코드 */}
+        <Layout.FlexCol>
+          <Font.Body type="12_regular" color="SYSTEM_ERROR" ml={2}>
+            123
+          </Font.Body>
+          <Font.Display type="24_bold">123</Font.Display>
+        </Layout.FlexCol>
+        <Layout.FlexRow>
+          <Button.Large type="white_fill" text="버튼" status="normal" />
+        </Layout.FlexRow>
+        <Logo>WhoAmI Today</Logo>
+        <Noti to="/notifications">noti</Noti>
       </Layout.FlexRow>
-      <Logo>WhoAmI Today</Logo>
-      <Noti to="/notifications">noti</Noti>
     </HeaderWrapper>
   );
 }
