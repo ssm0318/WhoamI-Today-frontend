f925bed758f36fdb72f1e0013481917831705d12
Gina Park <koyrkr@gmail.com>
2023-05-13 13:23:41 +0900

    fix: remove unnecessary file extension

--- src/design-system/SvgIcon/icons.ts
+++ src/design-system/SvgIcon/icons.ts
@@ -1,11 +1,11 @@
 // 아이콘이 추가되면 여기에도 추가 해주세요
-const top_navigation_hamburger = 'top_navigation_hamburger.svg';
-const top_navigation_noti = 'top_navigation_noti.svg';
-const arrow_left = 'arrow_left.svg';
-const arrow_right = 'arrow_right.svg';
-const moment_emoji_normal = 'moment_emoji_normal.svg';
-const moment_pencil_normal = 'moment_pencil_normal.svg';
-const moment_photo_normal = 'moment_photo_normal.svg';
+const top_navigation_hamburger = 'top_navigation_hamburger';
+const top_navigation_noti = 'top_navigation_noti';
+const arrow_left = 'arrow_left';
+const arrow_right = 'arrow_right';
+const moment_emoji_normal = 'moment_emoji_normal';
+const moment_pencil_normal = 'moment_pencil_normal';
+const moment_photo_normal = 'moment_photo_normal';
 
 export {
   top_navigation_hamburger,
