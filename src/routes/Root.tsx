4665fd30dd9b26e373beb564159ecfe209370250
GooJinSun <gjinsun96@gmail.com>
2023-05-13 22:39:44 +0900

    feat: #4 check if user sign in by router loader, remove authorization header from axios instance

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,4 +1,4 @@
-import { Outlet, redirect } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from '@components/header/Header';
 import Tab from '@components/tab/Tab';
 import { BOTTOM_TABBAR_HEIGHT, TOP_NAVIGATION_HEIGHT } from '@constants/layout';
@@ -6,8 +6,6 @@ import { Layout } from '@design-system';
 import { MainWrapper, RootContainer } from '@styles/wrappers';
 
 function Root() {
-  redirect('/home');
-
   return (
     <Layout.FlexRow justifyContent="center" bgColor="BASIC_BLACK" h="100vh" w="100%">
       <RootContainer w="100%" h="100vh" bgColor="BASIC_WHITE">
