2e4f984c45b0e9e67a1e9a035ef0b6218eb22186
Gina <koyrkr@gmail.com>
2023-05-27 10:14:45 +0900

    (#26) [New Feature] Today's Questions 컴포넌트 작업 (#29)
    * feat: add prev & next logic in Todays Question component
    
    * chore: add TODO comment for currentDate

--- src/components/home/todays-questions/TodaysQuestions.tsx
+++ src/components/home/todays-questions/TodaysQuestions.tsx
@@ -1,16 +1,49 @@
+import { addDays, format, isAfter, isBefore, isEqual, isSameDay, subDays } from 'date-fns';
+import { useState } from 'react';
+import { TODAY_QUESTION_FIRST_DATE } from '@constants/question';
 import { Button, Font, Layout, SvgIcon } from '@design-system';
 
 const shortAnswer = `Let's talk about your own way to get refreshed.`;
 const multipleChoices = ['as I am introverted.', 'kl I am introverted.', 'jj I am introverted.'];
 
 function TodaysQuestions() {
+  // 오늘 날짜
+  const today = new Date();
+  // TODAY_QUESTION_FIRST_DATE와 today를 비교 후, 더 나중 날짜를 currentDate로 set
+  // TODAY_QUESTION_FIRST_DATE보다 이전에 접속하면 항상 TODAY_QUESTION_FIRST_DATE로 유지
+  // TODO currentDate 기준으로 나중에 오늘의 질문 data 요청할 예정
+  const [currentDate, setCurrentDate] = useState(
+    isBefore(today, TODAY_QUESTION_FIRST_DATE) ? TODAY_QUESTION_FIRST_DATE : today,
+  );
+
+  const moveToPrevDate = () => {
+    if (!isPrevButtonExists) return;
+    setCurrentDate(subDays(currentDate, 1));
+  };
+
+  const moveToNextDate = () => {
+    if (!isNextButtonExists) return;
+    setCurrentDate(addDays(currentDate, 1));
+  };
+
+  const isPrevButtonExists = isAfter(currentDate, TODAY_QUESTION_FIRST_DATE);
+  const isNextButtonExists = !(isAfter(currentDate, today) || isEqual(currentDate, today));
+
   return (
     <Layout.FlexCol w="100%" bgColor="BACKGROUND_COLOR" ph="default" pt={22} pb={100}>
       {/* 타이틀 */}
-      <Layout.FlexRow w="100%" justifyContent="space-between" alignItems="center">
-        <SvgIcon name="arrow_left" size={36} />
-        <Font.Display type="18_bold">{`Today's Questions`}</Font.Display>
-        <SvgIcon name="arrow_right" size={36} />
+      <Layout.FlexRow w="100%" justifyContent="space-between" alignItems="center" h={38}>
+        <Layout.LayoutBase onClick={moveToPrevDate} w={36} h={36}>
+          {isPrevButtonExists && <SvgIcon name="arrow_left" size={36} />}
+        </Layout.LayoutBase>
+        <Font.Display type="18_bold">
+          {isSameDay(currentDate, today) || isBefore(today, TODAY_QUESTION_FIRST_DATE)
+            ? `Today's Questions`
+            : format(currentDate, 'yyyy/MM/dd')}
+        </Font.Display>
+        <Layout.LayoutBase onClick={moveToNextDate} w={36} h={36}>
+          {isNextButtonExists && <SvgIcon name="arrow_right" size={36} />}
+        </Layout.LayoutBase>
       </Layout.FlexRow>
       {/* 컨텐츠 */}
       {/* Short Answer */}
