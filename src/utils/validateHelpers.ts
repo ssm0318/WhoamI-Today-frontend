c94f538c6ae3e699b897b909f9c92d0c182b7cab
koyrkr <koyrkr@gmail.com>
2023-05-29 04:21:30 +0900

    fix: change validation logic isEmoji

--- src/utils/validateHelpers.ts
+++ src/utils/validateHelpers.ts
@@ -1,5 +1,5 @@
 export const isEmojiOnly = (text: string) => {
-  // eslint-disable-next-line no-misleading-character-class
-  const pattern = /^(?:[\p{Emoji}\u2614\uFE0F\u{1F3F4}\u{1F3F3}]+\s*)+$/u;
+  const pattern =
+    /^(?:(?:[\u{1F000}-\u{1FFFF}\u{20000}-\u{3FFFF}\u{E0000}-\u{EFFFF}\u{FE000}-\u{FEFFF}\u{FF000}-\u{FFFFF}])+\s*)+$/u;
   return pattern.test(text);
 };
