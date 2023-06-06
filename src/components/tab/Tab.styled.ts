73694f2546bae276e369a8c7ef2e6e55a01f411c
GooJinSun <gjinsun96@gmail.com>
2023-06-07 00:10:00 +0900

    feat: #no-issue add SVGIcon to Tab

--- src/components/tab/Tab.styled.ts
+++ src/components/tab/Tab.styled.ts
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { BOTTOM_TABBAR_HEIGHT, MAX_WINDOW_WIDTH } from '@constants/layout';
 
@@ -11,13 +11,14 @@ export const TabWrapper = styled.nav`
   margin: 0 auto;
   background-color: white;
   height: ${BOTTOM_TABBAR_HEIGHT}px;
+  border: 1px solid ${({ theme }) => theme.GRAY_10};
+  border-radius: 14px 14px 0px 0px;
 `;
 
-export const TabItem = styled(Link)`
+export const TabItem = styled(NavLink)`
   width: 100%;
   height: 100%;
-  text-decoration: none;
-  color: black;
-  border: 1px solid;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,11 +1,24 @@
+import { SvgIcon } from '@design-system';
 import { TabItem, TabWrapper } from './Tab.styled';
 
 function Tab() {
   return (
     <TabWrapper>
-      <TabItem to="/friends">Friends</TabItem>
-      <TabItem to="/home">Home</TabItem>
-      <TabItem to="/my">My</TabItem>
+      <TabItem to="/friends">
+        {({ isActive }) => (
+          <SvgIcon name={isActive ? 'friends_active' : 'friends_inactive'} size={36} color={null} />
+        )}
+      </TabItem>
+      <TabItem to="/home">
+        {({ isActive }) => (
+          <SvgIcon name={isActive ? 'home_active' : 'home_inactive'} size={48} color={null} />
+        )}
+      </TabItem>
+      <TabItem to="/my">
+        {({ isActive }) => (
+          <SvgIcon name={isActive ? 'my_active' : 'my_inactive'} size={36} color={null} />
+        )}
+      </TabItem>
     </TabWrapper>
   );
 }
