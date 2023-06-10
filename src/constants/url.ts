d3acb2e267388f8338c6b37cde2754073e1f0d28
GooJinSun <gjinsun96@gmail.com>
2023-06-10 17:00:17 +0900

    refact: #no-issue move external url to constants/url.ts

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,1 +1,9 @@
 export const DETAIL_DATE_PARAM_FORMAT = 'yyyy-MM-dd';
+export const PRIVACY_POLICY_AND_RESEARCH_CONSENT_FORM_NOTION_URL_EN =
+  'https://www.notion.so/jinsungoo/Privacy-Policy-Consent-Form-413087ddc0674afe9fc951e703236489';
+
+export const PRIVACY_POLICY_AND_RESEARCH_CONSENT_FORM_NOTION_URL_KO =
+  'https://jinsungoo.notion.site/fbbbfa2ea098431b86dbe75b320ea1c4';
+
+export const LEARN_MORE_ABOUT_WHOAMI_TODAY_NOTION_URL =
+  'https://www.notion.so/jinsungoo/What-is-WhoAmI-Today-TBU-7466713e676a42eca7d7d99c3ac341e9';
