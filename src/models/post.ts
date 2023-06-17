f84f7b0ea564f379e27c680c547e85187a5d5ca3
GooJinSun <gjinsun96@gmail.com>
2023-06-17 16:28:35 +0900

    refact: #no-issue add User interface, replace Author with User

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,3 +1,5 @@
+import { User } from './user';
+
 export enum QUESTION_TYPE {
   SHORT_ANSWER = 'SHORT_ANSWER',
 }
@@ -22,7 +24,7 @@ export interface ContentsCommon {
   id: number;
   content: string;
   author: string | null;
-  author_detail: Author | AdminAuthor;
+  author_detail: User | AdminAuthor;
   like_count: number | null;
   current_user_liked: boolean;
   created_at: string;
@@ -45,15 +47,6 @@ export interface UserTag {
   tagged_username: string;
   offset: number;
   length: number;
-}
-
-// 작성자
-export interface Author {
-  id: number;
-  profile_image: null;
-  profile_pic: string;
-  url: string;
-  username: string;
 }
 
 // 어드민 작성자
