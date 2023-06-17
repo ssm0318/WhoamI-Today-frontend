516eec9971635d03762ca40a87d28c5f678eb0a7
koyrkr <koyrkr@gmail.com>
2023-06-18 00:44:24 +0900

    fix: remove loader in Notifications

--- src/routes/Notifications.tsx
+++ src/routes/Notifications.tsx
@@ -1,11 +1,9 @@
 import { useTranslation } from 'react-i18next';
-import Loader from '@components/_common/loader/Loader';
 import MainContainer from '@components/_common/main-container/MainContainer';
 import NotificationItem from '@components/notification/NotificationItem';
 import TitleHeader from '@components/title-header/TitleHeader';
 import { DEFAULT_MARGIN, TITLE_HEADER_HEIGHT } from '@constants/layout';
 import { Layout } from '@design-system';
-import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import { notification } from '@mock/notification';
 import { Notification } from '@models/notification';
 
@@ -14,12 +12,6 @@ function Notifications() {
 
   const notiList: Notification[] = [notification];
 
-  const fetchMoreNotifications = async () => {
-    console.log('fetch more');
-  };
-
-  const { targetRef, isLoading } = useInfiniteScroll<HTMLDivElement>(fetchMoreNotifications);
-
   return (
     <MainContainer>
       <TitleHeader title={t('title')} />
@@ -27,12 +19,6 @@ function Notifications() {
         {notiList.map((noti) => (
           <NotificationItem item={noti} key={noti.id} />
         ))}
-        <div ref={targetRef} />
-        {isLoading && (
-          <Layout.FlexRow w="100%" h={40}>
-            <Loader />
-          </Layout.FlexRow>
-        )}
       </Layout.FlexCol>
     </MainContainer>
   );
