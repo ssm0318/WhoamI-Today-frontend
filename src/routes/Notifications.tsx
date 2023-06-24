3fbf18e6f6efd205daa00c24405d9419f4f13c55
koyrkr <koyrkr@gmail.com>
2023-06-24 19:23:14 +0900

    feat: apply notification apis

--- src/routes/Notifications.tsx
+++ src/routes/Notifications.tsx
@@ -1,24 +1,46 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import Loader from '@components/_common/loader/Loader';
 import MainContainer from '@components/_common/main-container/MainContainer';
 import NotificationItem from '@components/notification/NotificationItem';
 import TitleHeader from '@components/title-header/TitleHeader';
 import { DEFAULT_MARGIN, TITLE_HEADER_HEIGHT } from '@constants/layout';
 import { Layout } from '@design-system';
-import { notificationList } from '@mock/notification';
+import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import { Notification } from '@models/notification';
+import { getNotifications } from '@utils/apis/notification';
 
 function Notifications() {
   const [t] = useTranslation('translation', { keyPrefix: 'notifications' });
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [nextPage, setNextPage] = useState<string | null | undefined>(undefined);
 
-  const notiList: Notification[] = [...notificationList];
+  const fetchNotifications = async (page: string | null) => {
+    const { results, next } = await getNotifications(page);
+    if (!results) return;
+    setNextPage(next);
+    setNotifications([...notifications, ...results]);
+    setIsLoading(false);
+  };
+
+  const { isLoading, targetRef, setIsLoading } = useInfiniteScroll<HTMLDivElement>(async () => {
+    if (nextPage === null) return setIsLoading(false);
+    await fetchNotifications(nextPage === undefined ? null : nextPage);
+  });
 
   return (
     <MainContainer>
       <TitleHeader title={t('title')} />
       <Layout.FlexCol mt={TITLE_HEADER_HEIGHT} pv={14} w="100%" ph={DEFAULT_MARGIN} gap={20}>
-        {notiList.map((noti) => (
+        {notifications.map((noti) => (
           <NotificationItem item={noti} key={noti.id} />
         ))}
+        <div ref={targetRef} />
+        {isLoading && (
+          <Layout.FlexRow w="100%" h={40}>
+            <Loader />
+          </Layout.FlexRow>
+        )}
       </Layout.FlexCol>
     </MainContainer>
   );
--- src/utils/apis/notification.ts
+++ src/utils/apis/notification.ts
@@ -1,8 +1,12 @@
 import { PaginationResponse } from '@models/api/common';
+import { Notification } from '@models/notification';
 import axios from './axios';
 
 // GET notifications
-export const getNotifications = async () => {
-  const { data } = await axios.get<PaginationResponse<Notification[]>>(`/notifications/`);
+export const getNotifications = async (page: string | null) => {
+  const requestPage = page ? page.split('page=')[1] : null;
+  const { data } = await axios.get<PaginationResponse<Notification[]>>(
+    `/notifications/${!requestPage ? '' : `?page=${requestPage}`}`,
+  );
   return data;
 };
