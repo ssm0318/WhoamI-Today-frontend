c031af5d218001b5488ee915ea3f6335235b1a1e
koyrkr <koyrkr@gmail.com>
2023-06-25 11:59:12 +0900

    feat: maek getYearMonthDayFromDate util & apply

--- src/utils/timeHelpers.ts
+++ src/utils/timeHelpers.ts
@@ -34,3 +34,10 @@ export const convertTimeDiffByString = (now: Date, day: Date, dateFormat?: DateF
 
   return format(new Date(day), dateFormat ?? DEFAULT_FORMAT);
 };
+
+export const getYearMonthDayFromDate = (date: Date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return { year, month, day };
+};
