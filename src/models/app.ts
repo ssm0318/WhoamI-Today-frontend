e0f3f60d4b31eb12b9f6688c952711f92e8f99eb
koyrkr <koyrkr@gmail.com>
2023-07-15 18:49:40 +0900

    fix: remove unnecessary key

--- src/models/app.ts
+++ src/models/app.ts
@@ -1,9 +1,3 @@
-export interface SetTokenData {
-  key: 'SET_TOKEN';
-  access: string;
-  refresh: string;
-}
-
 export interface SetNotiPermissionData {
   key: 'SET_NOTI_PERMISSION';
   value: boolean;
@@ -20,17 +14,12 @@ export interface OpenSettingData {
 }
 
 // 앱, 웹 서로 약속한 키값에 따른 메시지 타입
-export type PostMessageDataType =
-  | SetTokenData
-  | ScreenNavigateData
-  | SetNotiPermissionData
-  | OpenSettingData;
+export type PostMessageDataType = ScreenNavigateData | SetNotiPermissionData | OpenSettingData;
 // 앱, 웹 서로 약속한 키값
 export type PostMessageKeyType = PostMessageDataType['key'];
 
 // Here we create a mapping of keys to types
 export type PostMessageKeyToData = {
-  SET_TOKEN: SetTokenData;
   NAVIGATE: ScreenNavigateData;
   SET_NOTI_PERMISSION: SetNotiPermissionData;
   OPEN_SETTING: OpenSettingData;
