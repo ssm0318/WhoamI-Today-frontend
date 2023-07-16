23fac329898858d2856ccf6da6c0ecbcc75bda4d
koyrkr <koyrkr@gmail.com>
2023-07-16 12:17:59 +0900

    feat: add isApp

--- src/utils/getUserAgent.ts
+++ src/utils/getUserAgent.ts
@@ -3,6 +3,8 @@ export const isMac = (userAgent = window?.navigator.userAgent) => {
   return /Mac/i.test(userAgent);
 };
 
+export const isApp: boolean = window?.ReactNativeWebView || false;
+
 export const getMobileDeviceInfo = (userAgent = window?.navigator.userAgent) => {
   // isMobile : 앱, 모바일 웹 모두 해당
   const isMobile = /Mobile/i.test(userAgent);
