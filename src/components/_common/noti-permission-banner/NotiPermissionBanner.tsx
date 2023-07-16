bd7cd844e548107c129d63299a658be86f097157
koyrkr <koyrkr@gmail.com>
2023-07-16 12:15:53 +0900

    feat: add useNotiPermission hook for descriptions

--- src/components/_common/noti-permission-banner/NotiPermissionBanner.tsx
+++ src/components/_common/noti-permission-banner/NotiPermissionBanner.tsx
@@ -1,5 +1,5 @@
-import { useTranslation } from 'react-i18next';
 import { Font } from '@design-system';
+import useNotiPermission from '@hooks/useNotiPermission';
 import * as S from './NotiPermissionBanner.styled';
 
 interface NotiPermissionBannerProps {
@@ -7,14 +7,18 @@ interface NotiPermissionBannerProps {
 }
 
 function NotiPermissionBanner({ onClick }: NotiPermissionBannerProps) {
-  const [t] = useTranslation('translation', { keyPrefix: 'settings' });
+  const { getBannerDescription } = useNotiPermission();
 
+  const descriptions = getBannerDescription(Notification.permission);
+
+  if (Notification.permission !== 'default' || descriptions.length === 0) return null;
   return (
     <S.Container justifyContent="center" bgColor="GRAY_10" h="50px" w="100%" onClick={onClick}>
-      <Font.Body type="14_regular" underline>
-        {t('allow_notification_to_receive_updates_from_friends_as_push_notifications')}
-      </Font.Body>
-      <Font.Body type="14_regular">⚠️ {t('please_check_your_system_preferences')} ⚠️</Font.Body>
+      {descriptions.map((desc) => (
+        <Font.Body type="14_regular" key={desc}>
+          {desc}
+        </Font.Body>
+      ))}
     </S.Container>
   );
 }
