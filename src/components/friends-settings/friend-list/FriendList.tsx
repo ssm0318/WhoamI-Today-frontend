2ad1b832c00014336210784dddb7b9fedc46765d
july001 <sk92983013@gmail.com>
2023-07-23 08:22:23 +0900

    feat: #111 Get friend list in FriendList component.

--- src/components/friends-settings/friend-list/FriendList.tsx
+++ src/components/friends-settings/friend-list/FriendList.tsx
@@ -1,20 +1,33 @@
+import { useEffect } from 'react';
+import Loader from '@components/_common/loader/Loader';
 import FriendItem from '@components/friends-settings/friend-item/FriendItem';
 import { Font, Layout } from '@design-system';
-import { friendList } from '@mock/friends';
+import { useBoundStore } from '@stores/useBoundStore';
+import { UserSelector } from '@stores/user';
 
 function FriendList() {
-  // TODO: GET 친구 목록
+  const { friendList, getFriendList } = useBoundStore(UserSelector);
+
+  useEffect(() => {
+    getFriendList();
+  }, [getFriendList]);
 
   return (
     <Layout.FlexCol w="100%" pl={10} pr={10} gap={8}>
-      <Font.Body type="14_regular" color="GRAY_12" ml={5} mt={14} mb={2}>
-        {friendList.length} people
-      </Font.Body>
-      <Layout.FlexCol w="100%" gap={8}>
-        {friendList.map((friend) => (
-          <FriendItem key={friend.id} type="friends" friend={friend} />
-        ))}
-      </Layout.FlexCol>
+      {friendList ? (
+        <>
+          <Font.Body type="14_regular" color="GRAY_12" ml={5} mt={14} mb={2}>
+            {friendList.length} people
+          </Font.Body>
+          <Layout.FlexCol w="100%" gap={8}>
+            {friendList.map((friend) => (
+              <FriendItem key={friend.id} type="friends" friend={friend} />
+            ))}
+          </Layout.FlexCol>
+        </>
+      ) : (
+        <Loader />
+      )}
     </Layout.FlexCol>
   );
 }
