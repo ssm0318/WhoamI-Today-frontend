265da4895957949b8f7c5e6a17cce8360d8ff764
july001 <sk92983013@gmail.com>
2023-07-22 13:55:17 +0900

    feat: #111 친구 목록 마크업

--- src/components/friends-settings/friend-list/FriendList.tsx
+++ src/components/friends-settings/friend-list/FriendList.tsx
@@ -1,10 +1,40 @@
-import { Layout } from '@design-system';
+import DeleteButton from '@components/_common/delete-button/DeleteButton';
+import ProfileImage from '@components/_common/profile-image/ProfileImage';
+import { Font, Layout } from '@design-system';
+import { friendList } from '@mock/friends';
+import { User } from '@models/user';
 
 function FriendList() {
+  // TODO: GET 친구 목록
+
+  const onClickDeleteFriend = (friend: User) => () => {
+    console.log('친구 삭제', friend.id);
+  };
+
   return (
-    <Layout.FlexCol>
-      {/* TODO: 친구 숫자 */}
-      {/* TODO: 친구 목록 */}
+    <Layout.FlexCol w="100%">
+      <Font.Body type="14_regular" color="GRAY_12" ml={15} mt={14} mb={12}>
+        {friendList.length} people
+      </Font.Body>
+      <Layout.FlexCol w="100%" gap={8}>
+        {friendList.map((friend) => (
+          <Layout.FlexRow
+            w="100%"
+            key={friend.id}
+            justifyContent="space-between"
+            pl={10}
+            pt={4}
+            pr={10}
+            pb={4}
+          >
+            <Layout.FlexRow alignItems="center" gap={7}>
+              <ProfileImage imageUrl={friend.profile_image} username={friend.username} size={42} />
+              <Font.Body type="14_semibold">{friend.username}</Font.Body>
+            </Layout.FlexRow>
+            <DeleteButton onClick={onClickDeleteFriend(friend)} />
+          </Layout.FlexRow>
+        ))}
+      </Layout.FlexCol>
     </Layout.FlexCol>
   );
 }
--- src/routes/settings/FriendsSettings.tsx
+++ src/routes/settings/FriendsSettings.tsx
@@ -1,16 +1,22 @@
 import { useTranslation } from 'react-i18next';
+import Divider from '@components/_common/divider/Divider';
 import MainContainer from '@components/_common/main-container/MainContainer';
 import FriendList from '@components/friends-settings/friend-list/FriendList';
 import TitleHeader from '@components/title-header/TitleHeader';
+import { TITLE_HEADER_HEIGHT } from '@constants/layout';
+import { Layout } from '@design-system';
 
 function FriendsSettings() {
   const [t] = useTranslation('translation', { keyPrefix: 'settings.friends' });
   return (
     <MainContainer>
       <TitleHeader title={t('title')} />
-      {/* TODO: 친구 검색 */}
-      {/* TODO: 친구 초대 */}
-      <FriendList />
+      <Layout.FlexCol mt={TITLE_HEADER_HEIGHT} w="100%">
+        {/* TODO: 친구 검색 */}
+        {/* TODO: 친구 초대 */}
+        <Divider width={1} />
+        <FriendList />
+      </Layout.FlexCol>
     </MainContainer>
   );
 }
