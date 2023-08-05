98598eafb114c18d62b3d21eb15993a04cc02934
koyrkr <koyrkr@gmail.com>
2023-08-06 02:28:01 +0900

    fix: not refer Notification when isApp

--- src/components/_common/noti-permission-banner/NotiPermissionBanner.tsx
+++ src/components/_common/noti-permission-banner/NotiPermissionBanner.tsx
@@ -1,13 +1,12 @@
 import { Font } from '@design-system';
 import useNotiPermission from '@hooks/useNotiPermission';
 import { requestPermission } from '@utils/firebaseHelpers';
-import { isApp } from '@utils/getUserAgent';
 import * as S from './NotiPermissionBanner.styled';
 
 function NotiPermissionBanner() {
   const { getBannerDescription, notiPermission, setNotiPermission } = useNotiPermission();
 
-  const descriptions = getBannerDescription(!isApp ? Notification.permission : undefined);
+  const descriptions = getBannerDescription(notiPermission);
 
   const handleRequest = async () => {
     const permission = await requestPermission();
