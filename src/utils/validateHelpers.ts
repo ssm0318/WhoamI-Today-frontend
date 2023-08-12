d7a0b00deec3adb024de5ce2b7003dc24446c01a
koyrkr <koyrkr@gmail.com>
2023-08-13 02:47:19 +0900

    chore: add short comment for fn

--- src/utils/validateHelpers.ts
+++ src/utils/validateHelpers.ts
@@ -18,6 +18,7 @@ export const objectFormDataSerializer = (object: Record<string, any>) => {
   return formData;
 };
 
+// object끼리 deep equal 비교
 export const deepEqual = (obj1: any, obj2: any): boolean => {
   if (obj1 === obj2) {
     return true;
