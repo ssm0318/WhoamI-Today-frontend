08434a508b3a6338e724bc00f661e89328042d17
july001 <sk92983013@gmail.com>
2023-08-12 20:31:27 +0900

    refact: #129 중복 스타일 제거

--- src/components/_common/toast-message/ToastMessage.styled.ts
+++ src/components/_common/toast-message/ToastMessage.styled.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Layout } from '@design-system';
 
 export const ToastContainer = styled(Layout.Absolute)`
-  position: absolute;
   display: table;
   max-width: 76%;
   margin: auto;
