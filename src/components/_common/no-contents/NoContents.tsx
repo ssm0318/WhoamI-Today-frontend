c4e53ee0b2530ab1e90e789940f0a443aedebea9
july001 <sk92983013@gmail.com>
2023-08-15 18:00:10 +0900

    feat: #137 add no friends case in SendQuestionModal.

--- src/components/_common/no-contents/NoContents.tsx
+++ src/components/_common/no-contents/NoContents.tsx
@@ -1,23 +1,32 @@
-import { Font, Layout } from '@design-system';
+import { ColorKeys, Font, Layout } from '@design-system';
 
 interface Props {
   title?: string | null;
   text?: string | null;
   ph?: number;
   mv?: number;
+  bgColor?: ColorKeys;
+  fontColor?: ColorKeys;
 }
 
-export default function NoContents({ title, text, ph = 20, mv = 10 }: Props) {
+export default function NoContents({
+  title,
+  text,
+  ph = 20,
+  mv = 10,
+  bgColor = 'GRAY_7',
+  fontColor = 'GRAY_12',
+}: Props) {
   return (
     <Layout.LayoutBase w="100%" alignItems="center" ph={ph} mv={mv}>
-      <Layout.FlexCol w="100%" alignItems="center" bgColor="GRAY_7" rounded={13} ph={10} pv={20}>
+      <Layout.FlexCol w="100%" alignItems="center" bgColor={bgColor} rounded={13} ph={10} pv={20}>
         {title && (
-          <Font.Body type="14_semibold" color="GRAY_12">
+          <Font.Body type="14_semibold" color={fontColor}>
             {title}
           </Font.Body>
         )}
         {text && (
-          <Font.Body type="14_semibold" color="GRAY_12">
+          <Font.Body type="14_regular" color={fontColor}>
             {text}
           </Font.Body>
         )}
