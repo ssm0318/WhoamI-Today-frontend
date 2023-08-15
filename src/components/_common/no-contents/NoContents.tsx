aa92e5f063666c7fb7c3ab27274d1f0b82860d77
july001 <sk92983013@gmail.com>
2023-08-15 15:11:48 +0900

    feat: #137 edit NoContents component.

--- src/components/_common/no-contents/NoContents.tsx
+++ src/components/_common/no-contents/NoContents.tsx
@@ -1,17 +1,26 @@
 import { Font, Layout } from '@design-system';
 
 interface Props {
+  title?: string | null;
   text?: string | null;
+  ph?: number;
 }
 
-export default function NoContents({ text }: Props) {
+export default function NoContents({ title, text, ph = 20 }: Props) {
   if (!text) return null;
   return (
-    <Layout.LayoutBase w="100%" alignItems="center" p={20} mv={10}>
-      <Layout.FlexCol w="100%" alignItems="center" bgColor="GRAY_7" rounded={12} ph={10} pv={20}>
-        <Font.Display type="14_semibold" color="GRAY_12">
-          {text}
-        </Font.Display>
+    <Layout.LayoutBase w="100%" alignItems="center" ph={ph} mv={10}>
+      <Layout.FlexCol w="100%" alignItems="center" bgColor="GRAY_7" rounded={13} ph={10} pv={20}>
+        {title && (
+          <Font.Body type="14_semibold" color="GRAY_12">
+            {title}
+          </Font.Body>
+        )}
+        {text && (
+          <Font.Body type="14_semibold" color="GRAY_12">
+            {text}
+          </Font.Body>
+        )}
       </Layout.FlexCol>
     </Layout.LayoutBase>
   );
