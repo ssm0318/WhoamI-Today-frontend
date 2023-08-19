d5e0d567ba7d7a620876a153f4dd6079056055f4
july001 <sk92983013@gmail.com>
2023-08-19 11:36:32 +0900

    feat: #152 my 페이지 배경컬러 변경

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -5,7 +5,7 @@ import { Layout } from '@design-system';
 
 function My() {
   return (
-    <Layout.FlexCol w="100%" pt={20}>
+    <Layout.FlexCol w="100%" h="100%" pt={20} bgColor="BACKGROUND_COLOR">
       <Layout.FlexRow w="100%" alignItems="center" justifyContent="space-between" ph={24} pb={14}>
         <MyProfile />
         <CalendarViewTabs />
