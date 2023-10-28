7ffeef403df603ec5af458f6a96e22d1818a74b7
july001 <sk92983013@gmail.com>
2023-10-28 15:13:24 +0900

    feat: #208 채팅방 목록 - 검색창 마크업

---
+++ src/components/chats/chat-room-search-input/ChatRoomSearchInput.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { CommonInput } from '@design-system';
+
+export const StyledSearchInput = styled(CommonInput)`
+  background: transparent;
+  border-bottom: none;
+  box-shadow: none;
+  padding: 0;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.GRAY_12};
+  }
+`;
--- src/routes/Chats.tsx
+++ src/routes/Chats.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import { ChatRoomSearchInput } from '@components/chats/chat-room-search-input/ChatRoomSearchInput';
 import { Layout } from '@design-system';
 
 function Chats() {
+  const [query, setQuery] = useState('');
+
   return (
     <Layout.FlexCol w="100%" justifyContent="flex-start" h="100vh" bgColor="BASIC_WHITE">
-      Chats
+      <ChatRoomSearchInput query={query} setQuery={setQuery} />
     </Layout.FlexCol>
   );
 }
