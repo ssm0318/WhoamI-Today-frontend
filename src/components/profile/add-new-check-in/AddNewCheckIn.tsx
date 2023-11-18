634e55ff60b63429073e71ae1b999e52e2d3bd20
koyrkr <koyrkr@gmail.com>
2023-11-18 20:08:59 +0900

    fix: fix NewAddCheckIn component

--- src/components/profile/add-new-check-in/AddNewCheckIn.tsx
+++ src/components/profile/add-new-check-in/AddNewCheckIn.tsx
@@ -2,7 +2,7 @@ import { Font, Layout, SvgIcon } from '@design-system';
 
 function AddNewCheckIn() {
   return (
-    <Layout.FlexRow gap={8} p={16} w="100%" alignItems="center" justifyContent="center">
+    <Layout.FlexRow gap={8} w="100%" alignItems="center" justifyContent="center">
       <SvgIcon name="new_add" size={24} />
       <Font.Body type="12_regular">New Check-in</Font.Body>
     </Layout.FlexRow>
