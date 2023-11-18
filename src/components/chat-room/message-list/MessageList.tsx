3036ade64aee4d19b291d91dcd48a667dd491037
july001 <sk92983013@gmail.com>
2023-11-18 15:22:21 +0900

    feat: #217 채팅방 입장시 스크롤을 맨 아래 설정

--- src/components/chat-room/message-list/MessageList.tsx
+++ src/components/chat-room/message-list/MessageList.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { MessageItem } from '@components/chat-room/message-item/MessageItem';
 import { MOCK_MESSAGE_LIST } from '@components/chat-room/message-list/MessageList.helper';
 import { Layout } from '@design-system';
@@ -9,14 +9,24 @@ export function MessageList() {
     return MOCK_MESSAGE_LIST;
   }, []);
 
-  // TODO: 처음 채팅방 진입시 스크롤 맨 아래로.
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  // TODO: 스크롤 맨 위로 올리면 이전 메시지 불러오기
+
+  // FIXME: 새 메시지 업데이트시 스크롤 아래로
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView();
+  }, []);
 
   return (
-    <Layout.FlexCol w="100%" gap={48} pv={15}>
-      {/* TODO: 날짜 분기 */}
-      {messageList.map((msg) => (
-        <MessageItem key={msg.msgId} {...msg} />
-      ))}
-    </Layout.FlexCol>
+    <>
+      <Layout.FlexCol w="100%" gap={48} pv={15}>
+        {/* TODO: 시스템 메시지, 날짜 표기 */}
+        {messageList.map((msg) => (
+          <MessageItem key={msg.msgId} {...msg} />
+        ))}
+      </Layout.FlexCol>
+      <div ref={bottomRef} />
+    </>
   );
 }
