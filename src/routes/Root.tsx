d74d9f0addc0963c19a7b5e83a90640b38095405
koyrkr <koyrkr@gmail.com>
2023-12-02 15:27:38 +0900

    fix: add SET_COOKIE message in PostMessageDataType

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import NotiPermissionBanner from '@components/_common/noti-permission-banner/NotiPermissionBanner';
 import Header from '@components/header/Header';
 import Tab from '@components/tab/Tab';
 import { BOTTOM_TABBAR_HEIGHT, TOP_NAVIGATION_HEIGHT } from '@constants/layout';
 import { Layout } from '@design-system';
+import { usePostAppMessage } from '@hooks/useAppMessage';
 import useAsyncEffect from '@hooks/useAsyncEffect';
 import useFcm from '@hooks/useFcm';
 import { MainWrapper, RootContainer } from '@styles/wrappers';
@@ -12,12 +14,19 @@ import { getMobileDeviceInfo } from '@utils/getUserAgent';
 function Root() {
   const { isMobile } = getMobileDeviceInfo();
   const { initializeFcm } = useFcm();
+  const postMessage = usePostAppMessage();
 
   useAsyncEffect(async () => {
     if (isMobile) return;
     // 데스크톱인 경우에만 initializeFcm
     await initializeFcm();
   }, [isMobile]);
+
+  useEffect(() => {
+    postMessage('SET_COOKIE', {
+      value: document.cookie,
+    });
+  }, [postMessage]);
 
   return (
     <Layout.FlexRow justifyContent="center" bgColor="BLACK" h="100vh" w="100%">
