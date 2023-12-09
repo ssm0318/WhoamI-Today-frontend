a943fe3d760aecd1f70a5c5a945372da9490993b
GooJinSun <gjinsun96@gmail.com>
2023-12-09 16:06:01 +0900

    feat: #251 <Collapse/> 컴포넌트 추가

---
+++ src/components/_common/collapse/Collapse.tsx
@@ -0,0 +1,32 @@
+import { ReactElement, useState } from 'react';
+import Icon from '@components/header/icon/Icon';
+import { Layout, Typo } from '@design-system';
+
+interface CollapseProps {
+  title: string;
+  collapsedItem: ReactElement;
+}
+
+function Collapse({ title, collapsedItem }: CollapseProps) {
+  const [open, setOpen] = useState(true);
+
+  const toggleCollapse = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <Layout.FlexRow w="100%" justifyContent="space-between" alignItems="center" ph={16}>
+        <Typo type="title-medium">{title}</Typo>
+        {open ? (
+          <Icon name="expand_close" size={28} onClick={toggleCollapse} />
+        ) : (
+          <Icon name="expand_open" size={28} onClick={toggleCollapse} />
+        )}
+      </Layout.FlexRow>
+      {open && collapsedItem}
+    </>
+  );
+}
+
+export default Collapse;
