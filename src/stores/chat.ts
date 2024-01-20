a84497e92d1e832da9e0ef1a1533c17ef93b8ea3
july001 <sk92983013@gmail.com>
2024-01-20 17:41:14 +0900

    feat: #264 채팅방 목록 페이지네이션 추가

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -1,13 +1,12 @@
 import { ChatRoom } from '@models/api/chat';
 import { FetchState } from '@models/api/common';
-import { getChatRooms } from '@utils/apis/chat';
 import { SliceStateCreator } from './useBoundStore';
 
 interface ChatState {
   chatRoomList: FetchState<ChatRoom[]>;
 }
 interface ChatAction {
-  getChatRoomList: () => void;
+  setChatRoomList: (chatRoomList: FetchState<ChatRoom[]>) => void;
 }
 
 const initialState: ChatState = {
@@ -18,16 +17,7 @@ export type ChatSlice = ChatState & ChatAction;
 
 export const createChatSlice: SliceStateCreator<ChatSlice> = (set) => ({
   ...initialState,
-  getChatRoomList: async () => {
-    try {
-      const chatRoomList = await getChatRooms();
-      set(
-        () => ({ chatRoomList: { state: 'hasValue', data: chatRoomList } }),
-        false,
-        'chat/getChatRoomList',
-      );
-    } catch {
-      set(() => ({ chatRoomList: { state: 'hasError' } }));
-    }
+  setChatRoomList: (chatRoomList) => {
+    set(() => ({ chatRoomList }), false, 'chat/setChatRoomList');
   },
 });
--- src/utils/apis/chat.ts
+++ src/utils/apis/chat.ts
@@ -2,9 +2,13 @@ import { ChatMessage, ChatRoom } from '@models/api/chat';
 import { PaginationResponse } from '@models/api/common';
 import axios from './axios';
 
-export const getChatRooms = async () => {
-  const { data } = await axios.get<PaginationResponse<ChatRoom[]>>(`/chat/rooms/`);
-  return data?.results || [];
+export const getChatRooms = async (next?: string | null) => {
+  const requestPage = next ? next.split('page=')[1] : null;
+
+  const { data } = await axios.get<PaginationResponse<ChatRoom[]>>(
+    `/chat/rooms/${requestPage ? `?page=${requestPage}` : ''}`,
+  );
+  return data;
 };
 
 export const getChatMessages = async (roomId: string | number, next?: string | null) => {
