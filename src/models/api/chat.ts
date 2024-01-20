af91dae4d5ccfbc60766b229d2fb7262936c164d
july001 <sk92983013@gmail.com>
2024-01-20 12:30:23 +0900

    feat: #264 chat api 타입 정의

---
+++ src/models/api/chat.ts
@@ -0,0 +1,15 @@
+import { User } from '@models/user';
+
+export interface ChatRoom {
+  id: number;
+  participants: User[];
+  last_message_content: string; // prompt type의 경우에는?
+  last_message_time: string;
+}
+
+export interface ChatMessage {
+  id: number;
+  sender: User;
+  content: string;
+  timestamp: string;
+}
--- src/utils/apis/chat.ts
+++ src/utils/apis/chat.ts
@@ -1,12 +1,13 @@
+import { ChatMessage, ChatRoom } from '@models/api/chat';
 import { PaginationResponse } from '@models/api/common';
 import axios from './axios';
 
 export const getChatRooms = async () => {
-  const { data } = await axios.get<PaginationResponse<any[]>>(`/chat/rooms/`);
+  const { data } = await axios.get<PaginationResponse<ChatRoom[]>>(`/chat/rooms/`);
   return data?.results || [];
 };
 
 export const getChatMessages = async (roomId: string) => {
-  const { data } = await axios.get<PaginationResponse<any[]>>(`/chat/${roomId}/messages/`);
+  const { data } = await axios.get<PaginationResponse<ChatMessage[]>>(`/chat/${roomId}/messages/`);
   return data?.results || [[]];
 };
