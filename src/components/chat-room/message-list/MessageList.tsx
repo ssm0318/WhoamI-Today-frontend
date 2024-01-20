cdd7c8d291a5bd0eb69db967491c379938cdf5f8
july001 <sk92983013@gmail.com>
2024-01-20 17:28:08 +0900

    refact: #264 불필요한 코드 제거

--- src/components/chat-room/message-list/MessageList.tsx
+++ src/components/chat-room/message-list/MessageList.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { MessageItem } from '@components/chat-room/message-item/MessageItem';
 import { Layout } from '@design-system';
 import { ChatRoom, SocketMessage } from '@models/api/chat';
@@ -11,11 +11,6 @@ interface Props {
 export function MessageList({ room, messages }: Props) {
   const bottomRef = useRef<HTMLDivElement>(null);
 
-  // 처음 입장시 스크롤 맨 아래로
-  useLayoutEffect(() => {
-    bottomRef.current?.scrollIntoView();
-  }, []);
-
   return (
     <Layout.FlexCol w="100%" gap={48} pv={15}>
       {/* TODO: 시스템 메시지, 날짜 표기 */}
@@ -23,6 +18,7 @@ export function MessageList({ room, messages }: Props) {
         const key = `${msg.timestamp}_${index}`;
         return <MessageItem key={key} room={room} message={msg} />;
       })}
+      <div ref={bottomRef} />
     </Layout.FlexCol>
   );
 }
