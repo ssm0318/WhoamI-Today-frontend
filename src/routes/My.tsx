e6d41024eadf91c191522371eeb32be7608c6cad
Gina <koyrkr@gmail.com>
2024-02-09 17:33:17 +0900

    fix: change My section style

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Divider from '@components/_common/divider/Divider';
 import Loader from '@components/_common/loader/Loader';
 import NoContents from '@components/_common/no-contents/NoContents';
-import Calendar from '@components/calendar/Calendar';
-import CalendarViewTabs from '@components/calendar/calendar-view-Tabs/CalendarViewTabs';
-import MyDetail from '@components/my-detail/MyDetail';
 import NoteSection from '@components/note/note-section/NoteSection';
 import Profile from '@components/profile/Profile';
 import ReactionSection from '@components/reaction/reaction-section/ReactionSection';
@@ -19,20 +16,11 @@ import { getResponseHistories } from '@utils/apis/question';
 
 function My() {
   const { myProfile } = useBoundStore((state) => ({ myProfile: state.myProfile }));
-
-  const { resetDetailDate, detailDate } = useBoundStore((state) => ({
-    resetDetailDate: state.resetDetailDate,
-    detailDate: state.detailDate,
-  }));
   const [t] = useTranslation('translation', { keyPrefix: 'no_contents' });
 
   const [responseHistory, setResponseHistory] = useState<FetchState<GetResponseHistoriesResponse>>({
     state: 'loading',
   });
-
-  useEffect(() => {
-    return () => resetDetailDate();
-  }, [resetDetailDate]);
 
   // TODO response 모두 가져올 수 있는 api 추가
   useAsyncEffect(async () => {
@@ -57,19 +45,7 @@ function My() {
         <Profile user={myProfile} />
       </Layout.FlexRow>
       <Divider width={1} />
-      <Layout.FlexRow
-        w="100%"
-        alignItems="center"
-        justifyContent="flex-end"
-        ph={DEFAULT_MARGIN}
-        pv={12}
-      >
-        <CalendarViewTabs />
-      </Layout.FlexRow>
-      <Calendar />
-      <MyDetail detailDate={detailDate} />
-
-      <Layout.FlexCol p={12} w="100%">
+      <Layout.FlexCol pv={12} pl={12} w="100%">
         <NoteSection />
       </Layout.FlexCol>
       <Divider width={1} />
