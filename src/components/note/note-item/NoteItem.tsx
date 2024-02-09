f2ad958697b980a3a2e01d49b83069c2be37f5b3
Gina <koyrkr@gmail.com>
2024-02-09 18:18:14 +0900

    fix: add more text when long

--- src/components/note/note-item/NoteItem.tsx
+++ src/components/note/note-item/NoteItem.tsx
@@ -1,11 +1,10 @@
-import { MouseEvent } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import IconButton from '@components/_common/icon-button/IconButton';
 import LikeButton from '@components/_common/like-button/LikeButton';
 import { SCREEN_WIDTH } from '@constants/layout';
-import { Font, Layout } from '@design-system';
+import { Layout, Typo } from '@design-system';
 import { Note } from '@models/note';
-import { useBoundStore } from '@stores/useBoundStore';
 import { convertTimeDiffByString } from '@utils/timeHelpers';
 import { NEW_NOTE_BUTTON_WIDTH } from '../new-note-button/NewNoteButton';
 
@@ -16,10 +15,7 @@ interface NoteItemProps {
 function NoteItem({ note }: NoteItemProps) {
   const { content, created_at, id } = note;
   const navigate = useNavigate();
-
-  const myProfile = useBoundStore((state) => state.myProfile);
-
-  if (!myProfile) return null;
+  const [overflowActive, setOverflowActive] = useState<boolean>(false);
 
   const handleClickMore = (e: MouseEvent) => {
     e.stopPropagation();
@@ -34,6 +30,12 @@ function NoteItem({ note }: NoteItemProps) {
     e.stopPropagation();
   };
 
+  useEffect(() => {
+    if (content.length > MAX_NOTE_CONTENT_LENGTH) {
+      setOverflowActive(true);
+    }
+  }, [content]);
+
   return (
     <Layout.FlexCol
       w={NOTE_WIDTH}
@@ -44,16 +46,29 @@ function NoteItem({ note }: NoteItemProps) {
       onClick={handleClickNote}
     >
       <Layout.FlexRow w="100%" justifyContent="space-between" alignItems="center">
-        <Font.Body type="12_regular" color="MEDIUM_GRAY">
+        <Typo type="label-medium" color="MEDIUM_GRAY">
           {convertTimeDiffByString(new Date(), new Date(created_at))}
-        </Font.Body>
+        </Typo>
         <IconButton name="dots_menu" onClick={handleClickMore} />
       </Layout.FlexRow>
-      <Layout.FlexRow>
-        <Font.Body type="16_regular" color="BLACK" numberOfLines={4}>
-          {content}
-        </Font.Body>
-      </Layout.FlexRow>
+      <Layout.FlexCol
+        style={{
+          position: 'relative',
+        }}
+      >
+        <Typo type="body-large" color="BLACK">
+          {overflowActive ? (
+            <>
+              {`${content.slice(0, MAX_NOTE_CONTENT_LENGTH)}...`}
+              <Typo type="body-medium" color="BLACK" italic underline ml={3}>
+                more
+              </Typo>
+            </>
+          ) : (
+            content
+          )}
+        </Typo>
+      </Layout.FlexCol>
       <Layout.FlexRow gap={12}>
         <LikeButton postType="Note" post={note} iconSize={24} m={0} />
         <IconButton name="comment" onClick={handleClickComment} />
@@ -65,5 +80,7 @@ function NoteItem({ note }: NoteItemProps) {
 const NOTE_GAP = 16;
 const NOTE_MARGIN = 12;
 export const NOTE_WIDTH = SCREEN_WIDTH - NEW_NOTE_BUTTON_WIDTH - 4 * NOTE_MARGIN - NOTE_GAP * 2;
+export const NOTE_HEIGHT = 144;
 
+const MAX_NOTE_CONTENT_LENGTH = 140;
 export default NoteItem;
