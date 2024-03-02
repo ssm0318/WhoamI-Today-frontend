23ba9425033ea54a8123445bfb222e519012a876
july001 <sk92983013@gmail.com>
2024-03-02 16:15:52 +0900

    feat: #269 BE에서 chat 브랜치가 머지되기 전에 목데이터를 사용하도록 수정

--- src/components/chats/chat-room-list/ChatRoomList.tsx
+++ src/components/chats/chat-room-list/ChatRoomList.tsx
@@ -1,13 +1,8 @@
-import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import Loader from '@components/_common/loader/Loader';
 import { SwipeLayoutList } from '@components/_common/swipe-layout/SwipeLayoutList';
 import { ChatRoomItem } from '@components/chats/chat-room-list/ChatRoomItem';
 import { Layout, Typo } from '@design-system';
-import useAsyncEffect from '@hooks/useAsyncEffect';
-import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import { useBoundStore } from '@stores/useBoundStore';
-import { getChatRooms } from '@utils/apis/chat';
 import { SwipeableChatRoomItem } from './SwipeableChatRoomItem';
 
 interface Props {
@@ -17,35 +12,36 @@ interface Props {
 export function ChatRoomList({ isEditMode }: Props) {
   const [t] = useTranslation('translation', { keyPrefix: 'chats.room_list' });
 
-  const { rooms, setRooms } = useBoundStore((state) => ({
+  const { rooms } = useBoundStore((state) => ({
     rooms: state.chatRoomList,
     setRooms: state.setChatRoomList,
   }));
 
-  const [nextUrl, setNextUrl] = useState<string | null>(null);
+  // TODO: BE에서 chat api 머지 후 주석 해제
+  // const [nextUrl, setNextUrl] = useState<string | null>(null);
 
-  const fetchChatRoomList = useCallback(
-    async (_next?: string) => {
-      try {
-        const { results = [], next } = await getChatRooms(_next);
-        setRooms({ state: 'hasValue', data: results });
-        setNextUrl(next);
-      } catch {
-        setRooms({ state: 'hasError' });
-      }
-    },
-    [setRooms],
-  );
-  useAsyncEffect(fetchChatRoomList, [fetchChatRoomList]);
+  // const fetchChatRoomList = useCallback(
+  //   async (_next?: string) => {
+  //     try {
+  //       const { results = [], next } = await getChatRooms(_next);
+  //       setRooms({ state: 'hasValue', data: results });
+  //       setNextUrl(next);
+  //     } catch {
+  //       setRooms({ state: 'hasError' });
+  //     }
+  //   },
+  //   [setRooms],
+  // );
+  // useAsyncEffect(fetchChatRoomList, [fetchChatRoomList]);
 
-  const { isLoading, targetRef, setIsLoading } = useInfiniteScroll<HTMLDivElement>(async () => {
-    if (nextUrl) {
-      await fetchChatRoomList(nextUrl);
-      setIsLoading(false);
-      return;
-    }
-    setIsLoading(false);
-  });
+  // const { isLoading, targetRef, setIsLoading } = useInfiniteScroll<HTMLDivElement>(async () => {
+  //   if (nextUrl) {
+  //     await fetchChatRoomList(nextUrl);
+  //     setIsLoading(false);
+  //     return;
+  //   }
+  //   setIsLoading(false);
+  // });
 
   return (
     <Layout.FlexCol w="100%" h="100%" pv={5} gap={5}>
@@ -67,8 +63,8 @@ export function ChatRoomList({ isEditMode }: Props) {
               ))}
             </SwipeLayoutList>
           )}
-          <div ref={targetRef} />
-          {isLoading && <Loader />}
+          {/* <div ref={targetRef} />
+        {isLoading && <Loader />} */}
         </Layout.FlexCol>
       )}
     </Layout.FlexCol>
