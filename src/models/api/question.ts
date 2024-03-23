22ee83b60354f180e397c1dd786dc19ce3473991
GooJinSun <gjinsun96@gmail.com>
2024-03-23 15:16:26 +0900

    feat: #311 add post a response api

--- src/models/api/question.ts
+++ src/models/api/question.ts
@@ -3,6 +3,8 @@ import { Question, Response } from '@models/post';
 export interface ResponseQuestionRequestParams {
   question_id: number;
   content: string;
+  share_friends?: unknown;
+  share_groups?: unknown;
 }
 
 export interface GetResponseHistoriesResponse extends Question {
--- src/routes/response/NewResponse.tsx
+++ src/routes/response/NewResponse.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Loader } from '@components/_common/loader/Loader.styled';
 import MainContainer from '@components/_common/main-container/MainContainer';
 import ProfileImage from '@components/_common/profile-image/ProfileImage';
 import { StyledNewResponsePrompt } from '@components/_common/prompt/PromptCard.styled';
 import SubHeader from '@components/sub-header/SubHeader';
 import { TITLE_HEADER_HEIGHT } from '@constants/layout';
-import { Typo } from '@design-system';
+import { TextArea, Typo } from '@design-system';
 import useAsyncEffect from '@hooks/useAsyncEffect';
 import { FetchState } from '@models/api/common';
 import { Question } from '@models/post';
 import { useBoundStore } from '@stores/useBoundStore';
-import { getQuestionDetail } from '@utils/apis/question';
+import { getQuestionDetail, responseQuestion } from '@utils/apis/question';
 import { FlexRow, LayoutBase } from 'src/design-system/layouts';
 
 const isValidQuestionId = (questionId?: string): questionId is string =>
@@ -35,9 +35,42 @@ function NewResponse() {
       .catch((error) => ({ state: 'hasError', error }));
   }, []);
 
+  const [newResponse, setNewResponse] = useState('');
+  const handleChangeResponse = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setNewResponse(e.target.value);
+  };
+
+  const navigate = useNavigate();
+  const handleClickCancel = () => {
+    navigate(-1);
+  };
+
+  const handleClickPost = async () => {
+    if (!questionId) return;
+    await responseQuestion({ question_id: Number(questionId), content: newResponse });
+    // TODO: 포스팅 완료 토스트 연동
+    navigate('/my');
+  };
+
+  const disabledPost = !newResponse.trim().length;
+
   return (
     <MainContainer>
-      <SubHeader title={t('new_response')} />
+      <SubHeader
+        title={t('new_response')}
+        LeftComponent={
+          <button type="button" onClick={handleClickCancel}>
+            <Typo type="title-large">{t('cancel')}</Typo>
+          </button>
+        }
+        RightComponent={
+          <button type="button" onClick={handleClickPost} disabled={disabledPost}>
+            <Typo type="title-large" color={disabledPost ? 'MEDIUM_GRAY' : 'PRIMARY'}>
+              {t('post')}
+            </Typo>
+          </button>
+        }
+      />
       <LayoutBase mt={TITLE_HEADER_HEIGHT} w="100%" pt={20} ph={12}>
         {currentUser && (
           <FlexRow gap={4} alignItems="center" mb={12}>
@@ -49,15 +82,23 @@ function NewResponse() {
             <Typo type="title-medium">{currentUser.username}</Typo>
           </FlexRow>
         )}
+
         {question.state === 'loading' && <Loader />}
         {question.state === 'hasValue' && (
-          <StyledNewResponsePrompt>
-            <FlexRow gap={8} alignItems="center" mb={12}>
-              <ProfileImage imageUrl="/whoami-profile.svg" username="Whoami Today" size={28} />
-              <Typo type="title-medium">Whoami Today</Typo>
-            </FlexRow>
-            <Typo type="body-large">{question.data.content}</Typo>
-          </StyledNewResponsePrompt>
+          <>
+            <TextArea
+              placeholder={t('what_is_your_response') || ''}
+              value={newResponse}
+              onChange={handleChangeResponse}
+            />
+            <StyledNewResponsePrompt>
+              <FlexRow gap={8} alignItems="center" mb={12}>
+                <ProfileImage imageUrl="/whoami-profile.svg" username="Whoami Today" size={28} />
+                <Typo type="title-medium">Whoami Today</Typo>
+              </FlexRow>
+              <Typo type="body-large">{question.data.content}</Typo>
+            </StyledNewResponsePrompt>
+          </>
         )}
       </LayoutBase>
     </MainContainer>
