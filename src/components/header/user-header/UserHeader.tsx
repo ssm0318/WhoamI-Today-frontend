f1d35e62d34df1a80b0a108c459259fd8bb3e90d
july001 <sk92983013@gmail.com>
2024-04-13 21:23:56 +0900

    feat: #327 친구페이지 헤더 수정

--- src/components/header/user-header/UserHeader.tsx
+++ src/components/header/user-header/UserHeader.tsx
@@ -1,63 +1,34 @@
-import { useNavigate } from 'react-router-dom';
 import Icon from '@components/_common/icon/Icon';
-import { Font, Layout } from '@design-system';
+import SubHeader from '@components/sub-header/SubHeader';
+import { Layout } from '@design-system';
 import { User } from '@models/user';
-import { useBoundStore } from '@stores/useBoundStore';
-import { HeaderWrapper } from '../Header.styled';
 
 interface UserHeaderProps {
-  onClickHamburger?: () => void;
   user?: User;
 }
 
-function UserHeader({ onClickHamburger, user }: UserHeaderProps) {
-  const navigate = useNavigate();
-  const { myProfile } = useBoundStore((state) => ({ myProfile: state.myProfile }));
-  const isMyPage = user?.id === myProfile?.id;
-
-  const handleEditCheckIn = () => {
-    // 수정 페이지로 이동
-    navigate('/check-in/edit');
-  };
-
+function UserHeader({ user }: UserHeaderProps) {
   const handleClickChat = () => {
-    //
+    // TODO: 유저와의 채팅방으로 이동
   };
 
   const handleClickMore = () => {
-    //
-  };
-
-  const handleGoBack = () => {
-    navigate(-1);
+    // TODO: 유저 더보기 모달 열기
   };
 
   if (!user) return null;
   return (
-    <HeaderWrapper>
-      <Layout.FlexRow justifyContent="space-between" w="100%" alignItems="center" pv={10}>
-        <Layout.FlexRow>
-          {isMyPage ? (
-            <Font.Display type="24_regular">My Profile</Font.Display>
-          ) : (
-            <Icon name="arrow_left" size={44} onClick={handleGoBack} />
-          )}
-        </Layout.FlexRow>
+    <SubHeader
+      title={user.username}
+      RightComponent={
         <Layout.FlexRow gap={8} alignItems="center">
-          {isMyPage ? (
-            <>
-              <Icon name="edit_outline" size={44} onClick={handleEditCheckIn} />
-              <Icon name="hamburger" size={44} onClick={onClickHamburger} />
-            </>
-          ) : (
-            <>
-              <Icon name="dots_menu" size={44} onClick={handleClickMore} />
-              <Icon name="chat_outline" size={44} onClick={handleClickChat} />
-            </>
-          )}
+          <Icon name="chat_outline" size={44} onClick={handleClickChat} />
+          <Layout.Absolute r={62}>
+            <Icon name="dots_menu" size={44} onClick={handleClickMore} />
+          </Layout.Absolute>
         </Layout.FlexRow>
-      </Layout.FlexRow>
-    </HeaderWrapper>
+      }
+    />
   );
 }
 
