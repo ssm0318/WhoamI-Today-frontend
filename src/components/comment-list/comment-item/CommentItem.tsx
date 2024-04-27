7d25ae4d36d7df1f84c18600f8a90c496f555ec6
Gina <koyrkr@gmail.com>
2024-04-27 15:39:19 +0900

    fix: apply replies

--- src/components/comment-list/comment-item/CommentItem.tsx
+++ src/components/comment-list/comment-item/CommentItem.tsx
@@ -12,12 +12,13 @@ import { convertTimeDiffByString } from '@utils/timeHelpers';
 
 interface CommentItemProps {
   comment: Comment;
+  replyAvailable?: boolean;
   onClickReplyBtn?: () => void;
 }
 
-function CommentItem({ comment, onClickReplyBtn }: CommentItemProps) {
+function CommentItem({ comment, onClickReplyBtn, replyAvailable = false }: CommentItemProps) {
   const [t] = useTranslation('translation', { keyPrefix: 'comment' });
-  const { author_detail, created_at, is_private } = comment;
+  const { author_detail, created_at, is_private, replies } = comment;
   const { username, imageUrl } = getAuthorProfileInfo(author_detail);
 
   const [createdAt] = useState(() => new Date(created_at));
@@ -35,67 +36,67 @@ function CommentItem({ comment, onClickReplyBtn }: CommentItemProps) {
   };
 
   return (
-    <Layout.FlexRow w="100%" justifyContent="space-between" alignItems="flex-start" gap={8}>
-      {/* Author Profile */}
-      <Layout.FlexCol w={30}>
-        <ProfileImage imageUrl={imageUrl} size={30} />
-      </Layout.FlexCol>
-      {/* Author name, time, content */}
-      <Layout.FlexCol flex={1} alignItems="center">
-        <Layout.FlexCol w="100%" gap={4}>
-          <Layout.FlexRow w="100%" alignItems="center">
-            {is_private && <Icon name="private_comment_active" size={16} />}
-            <Typo type="label-medium">{username}</Typo>
-            <Layout.FlexRow ml={8}>
-              <Typo type="label-small" color="MEDIUM_GRAY">
-                {convertTimeDiffByString(currentDate, createdAt, 'yyyy.MM.dd HH:mm', true)}
-              </Typo>
-            </Layout.FlexRow>
-          </Layout.FlexRow>
-          {/* TODO: 줄바꿈 표시 */}
-          <Typo pre type="body-medium">{`${comment.content}`}</Typo>
-          {/* Reply & Message buttons */}
-          <Layout.FlexRow w="100%" gap={7} alignItems="center">
-            <button type="button" onClick={toggleReplyInput}>
-              <Layout.FlexRow gap={4} alignItems="center">
-                <SvgIcon name="comment_reply" size={24} />
-                <Typo type="label-medium" color="DARK_GRAY">
-                  {t('reply')}
-                </Typo>
-              </Layout.FlexRow>
-            </button>
-            <button type="button" onClick={toggleReplyInput}>
-              <Layout.FlexRow gap={4} alignItems="center">
-                <SvgIcon name="comment_message" size={24} />
-                <Typo type="label-medium" color="DARK_GRAY">
-                  {t('message')}
-                </Typo>
-              </Layout.FlexRow>
-            </button>
-          </Layout.FlexRow>
+    <Layout.FlexCol w="100%">
+      <Layout.FlexRow w="100%" justifyContent="space-between" alignItems="flex-start" gap={8}>
+        {/* Author Profile */}
+        <Layout.FlexCol w={30}>
+          <ProfileImage imageUrl={imageUrl} size={30} />
         </Layout.FlexCol>
-      </Layout.FlexCol>
-      {/* <Layout.FlexRow w="100%" gap={2}>
+        {/* Author name, time, content */}
+        <Layout.FlexCol flex={1} alignItems="center">
+          <Layout.FlexCol w="100%" gap={4}>
+            <Layout.FlexRow w="100%" alignItems="center">
+              {is_private && <Icon name="private_comment_active" size={16} />}
+              <Typo type="label-medium">{username}</Typo>
+              <Layout.FlexRow ml={8}>
+                <Typo type="label-small" color="MEDIUM_GRAY">
+                  {convertTimeDiffByString(currentDate, createdAt, 'yyyy.MM.dd HH:mm', true)}
+                </Typo>
+              </Layout.FlexRow>
+            </Layout.FlexRow>
+            {/* TODO: 줄바꿈 표시 */}
+            <Typo pre type="body-medium">{`${comment.content}`}</Typo>
+            {/* Reply & Message buttons */}
+            <Layout.FlexRow w="100%" gap={7} alignItems="center">
+              {replyAvailable && (
+                <button type="button" onClick={toggleReplyInput}>
+                  <Layout.FlexRow gap={4} alignItems="center">
+                    <SvgIcon name="comment_reply" size={24} />
+                    <Typo type="label-medium" color="DARK_GRAY">
+                      {t('reply')}
+                    </Typo>
+                  </Layout.FlexRow>
+                </button>
+              )}
+              <button type="button" onClick={toggleReplyInput}>
+                <Layout.FlexRow gap={4} alignItems="center">
+                  <SvgIcon name="comment_message" size={24} />
+                  <Typo type="label-medium" color="DARK_GRAY">
+                    {t('message')}
+                  </Typo>
+                </Layout.FlexRow>
+              </button>
+            </Layout.FlexRow>
+          </Layout.FlexCol>
+        </Layout.FlexCol>
+        {/* more button / like button */}
+        <Layout.FlexCol w={24}>
+          {isCommentAuthor ? (
+            <Layout.FlexRow>
+              <Icon name="dots_menu" size={24} onClick={handleClickMore} />
+            </Layout.FlexRow>
+          ) : (
+            <LikeButton postType="Comment" post={comment} iconSize={15} />
+          )}
+        </Layout.FlexCol>
+      </Layout.FlexRow>
+      {/* replies */}
+      <Layout.FlexRow w="100%" gap={8} ml={20} mt={14}>
         {replies.map((reply) => (
-          <CommentItem
-            key={reply.id}
-            comment={reply}
-            onClickReplyBtn={toggleReplyInput}
-            onClickDeleteBtn={onClickDeleteBtn}
-          />
+          <CommentItem key={reply.id} comment={reply} replyAvailable={false} />
         ))}
-      </Layout.FlexRow> */}
-      {/* more button / like button */}
-      <Layout.FlexCol w={24}>
-        {isCommentAuthor ? (
-          <Layout.FlexRow>
-            <Icon name="dots_menu" size={24} onClick={handleClickMore} />
-          </Layout.FlexRow>
-        ) : (
-          <LikeButton postType="Comment" post={comment} iconSize={15} />
-        )}
-      </Layout.FlexCol>
-    </Layout.FlexRow>
+      </Layout.FlexRow>
+    </Layout.FlexCol>
   );
 }
 
