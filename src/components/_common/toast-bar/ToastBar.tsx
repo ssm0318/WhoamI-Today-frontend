ba6b7ea5edfef68268ec35ea74d9fdec5d993941
Yuri Jeon <48233023+July001@users.noreply.github.com>
2024-05-11 08:58:46 +0900

    (#369) 토스트바 컴포넌트가 사라지지 않는 버그 수정 (#389)
    * bugfix: #369 토스트바가 사라지지 않는 버그 수정
    
    * bugfix: #369 토스트 닫기 애니메이션 시간에 맞춰서 timeout 시간 변경

--- src/components/_common/toast-bar/ToastBar.tsx
+++ src/components/_common/toast-bar/ToastBar.tsx
@@ -26,7 +26,7 @@ export default function ToastBar({
       setAnimationState('hide');
       setTimeout(() => {
         closeToastBar?.();
-      }, timeout);
+      }, 500);
     }, timeout);
 
     return () => {
