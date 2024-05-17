381b01fd078a27816f814314709e2ae2f367b5e9
july001 <sk92983013@gmail.com>
2024-05-18 07:10:57 +0900

    feat: #377 mock data 제거 && api 연결

--- src/components/notification/received-prompt-list/ReceivedPromptList.tsx
+++ src/components/notification/received-prompt-list/ReceivedPromptList.tsx
@@ -1,11 +1,11 @@
 import RecentPromptCard from '@components/_common/prompt/RecentPromptCard';
 import { Layout, Typo } from '@design-system';
-import { Notification } from '@models/notification';
+import { ResponseRequest } from '@models/api/question';
 import { convertTimeDiffByString } from '@utils/timeHelpers';
 
 interface ReceivedPromptListProps {
   title: string;
-  prompts: Notification[];
+  prompts: ResponseRequest[];
 }
 
 export default function ReceivedPromptList({ title, prompts }: ReceivedPromptListProps) {
@@ -19,7 +19,6 @@ export default function ReceivedPromptList({ title, prompts }: ReceivedPromptLis
           </Typo>
         </Layout.FlexRow>
         <Layout.FlexCol w="100%" gap={12}>
-          {/* TODO: question 필드가 없어서 임시로 임의로 question을 만들어서 넘겨줌. 나중에 api 교체시 수정 필요! */}
           {prompts.map((n) => (
             <ReceivedPromptItem prompt={n} />
           ))}
@@ -30,19 +29,20 @@ export default function ReceivedPromptList({ title, prompts }: ReceivedPromptLis
 }
 
 interface ReceivedPromptItemProps {
-  prompt: Notification;
+  prompt: ResponseRequest;
 }
 
 function ReceivedPromptItem({ prompt }: ReceivedPromptItemProps) {
-  const { id, question_content, created_at, recent_actors } = prompt;
+  const { id, created_at, question_id } = prompt;
   return (
     <Layout.FlexRow w="100%" key={id} gap={4} alignItems="center">
+      {/* TODO: question 필드가 없어서 임시로 임의로 question을 만들어서 넘겨줌. 나중에 수정 필요! */}
       <RecentPromptCard
-        sentBy={recent_actors[0]}
+        requesterName="User!!"
         question={{
           type: 'Question',
-          id,
-          content: question_content,
+          id: question_id,
+          content: 'What was a funny thing that happened today?',
           created_at,
           is_admin_question: false,
           selected_dates: ['2024-05-10'],
