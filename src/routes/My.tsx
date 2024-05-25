d5840cfb2d689c6ffb380b01c70c06bb0755301f
Gina <koyrkr@gmail.com>
2024-05-25 17:20:51 +0900

    feat: add whats on your mind section

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -1,12 +1,23 @@
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import Divider from '@components/_common/divider/Divider';
+import Icon from '@components/_common/icon/Icon';
 import NoteSection from '@components/note/note-section/NoteSection';
 import Profile from '@components/profile/Profile';
 import ResponseSection from '@components/response/response-section/ResponseSection';
-import { Layout } from '@design-system';
+import { Layout, Typo } from '@design-system';
 import { useBoundStore } from '@stores/useBoundStore';
 
 function My() {
   const { myProfile } = useBoundStore((state) => ({ myProfile: state.myProfile }));
+  const [t] = useTranslation('translation', {
+    keyPrefix: 'my',
+  });
+  const navigate = useNavigate();
+
+  const handleClickNewNote = () => {
+    return navigate('/notes/new');
+  };
 
   return (
     <Layout.FlexCol w="100%" bgColor="LIGHT">
@@ -25,6 +36,24 @@ function My() {
       <Layout.FlexCol pv={12} pl={12} w="100%" bgColor="WHITE" rounded={8}>
         <ResponseSection />
       </Layout.FlexCol>
+      <Divider width={8} bgColor="LIGHT" />
+      <Layout.FlexRow ph={15} pv={10} bgColor="WHITE" w="100%">
+        <Layout.FlexRow
+          rounded={30}
+          alignItems="center"
+          w="100%"
+          justifyContent="space-between"
+          ph={20}
+          pv={10}
+          outline="LIGHT_GRAY"
+          onClick={handleClickNewNote}
+        >
+          <Typo type="body-medium" color="DARK_GRAY">
+            {t('whats_on_your_mind', { username: myProfile?.username })}
+          </Typo>
+          <Icon name="chat_media_image" size={24} />
+        </Layout.FlexRow>
+      </Layout.FlexRow>
       <Divider width={8} bgColor="LIGHT" />
       <Layout.FlexCol pt={12} pl={12} pb="default" w="100%" bgColor="WHITE" rounded={8}>
         <NoteSection />
