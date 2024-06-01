133992d57e38d5c6fec1b715122fc279ae005117
Gina <koyrkr@gmail.com>
2024-06-01 17:00:44 +0900

    (#446) Note/Response Likes 페이지 SearchInput 삭제 (#450)
    * fix: remove search input in likes page
    
    * fix: remove deprecated translations

--- src/routes/Likes.tsx
+++ src/routes/Likes.tsx
@@ -4,7 +4,6 @@ import { useNavigate, useParams } from 'react-router-dom';
 import Loader from '@components/_common/loader/Loader';
 import MainContainer from '@components/_common/main-container/MainContainer';
 import ProfileImage from '@components/_common/profile-image/ProfileImage';
-import SearchInput from '@components/_common/search-input/SearchInput';
 import SubHeader from '@components/sub-header/SubHeader';
 import { TITLE_HEADER_HEIGHT } from '@constants/layout';
 import { Layout, Typo } from '@design-system';
@@ -20,9 +19,6 @@ function Likes() {
   const [t] = useTranslation('translation', { keyPrefix: 'likes' });
   const [likes, setLikes] = useState<Like[]>([]);
   const [nextPage, setNextPage] = useState<string | null | undefined>(undefined);
-
-  // TODO : search query
-  const [query, setQuery] = useState('');
 
   const { isLoading, targetRef, setIsLoading } = useInfiniteScroll<HTMLDivElement>(async () => {
     if (nextPage === null) return setIsLoading(false);
@@ -51,15 +47,6 @@ function Likes() {
     <MainContainer>
       <SubHeader title={t('title')} />
       <Layout.FlexCol mt={TITLE_HEADER_HEIGHT} w="100%" ph={16}>
-        <Layout.LayoutBase pb={8} mt={20} bgColor="WHITE" w="100%">
-          <SearchInput
-            query={query}
-            setQuery={setQuery}
-            fontSize={16}
-            placeholder={t('search') || undefined}
-            cancelText={t('cancel') || undefined}
-          />
-        </Layout.LayoutBase>
         {/* like list */}
         {likes.map((like) => {
           const { id, user_detail } = like;
