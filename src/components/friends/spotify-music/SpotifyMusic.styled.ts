3d8b0074480a31aa47d347a2051d76c7cacce910
july001 <sk92983013@gmail.com>
2024-06-15 18:08:18 +0900

    feat: #440 친구 목록에서 스포티파이 영역이 클릭가능하도록 버튼 영역 추가

---
+++ src/components/friends/spotify-music/SpotifyMusic.styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledSpotifyMusic = styled.button`
+  display: 'flex';
+  flex-shrink: 0;
+`;
--- src/components/friends/spotify-music/SpotifyMusic.tsx
+++ src/components/friends/spotify-music/SpotifyMusic.tsx
@@ -2,6 +2,7 @@ import { Track } from '@spotify/web-api-ts-sdk';
 import { useEffect, useState } from 'react';
 import { Layout, SvgIcon, Typo } from '@design-system';
 import SpotifyManager from '@libs/SpotifyManager';
+import { StyledSpotifyMusic } from './SpotifyMusic.styled';
 
 interface Props {
   track_id: string | undefined;
@@ -16,23 +17,30 @@ function SpotifyMusic({ track_id }: Props) {
     spotifyManager.getTrack(track_id).then(setTrackData);
   }, [spotifyManager, track_id]);
 
+  const handleClickMusic = () => {
+    // TODO:
+  };
+
   return (
-    <Layout.FlexRow
-      outline="SPOTIFY_GREEN"
-      rounded={12}
-      justifyContent="center"
-      alignItems="center"
-      pv={4}
-      ph={8}
-      gap={4}
-    >
-      <SvgIcon name="spotify" size={16} />
-      {trackData && (
-        <Typo type="label-medium" numberOfLines={1}>
-          {trackData.artists[0].name} - {trackData.name}
-        </Typo>
-      )}
-    </Layout.FlexRow>
+    <StyledSpotifyMusic type="button" onClick={handleClickMusic}>
+      <Layout.FlexRow
+        w="100%"
+        outline="SPOTIFY_GREEN"
+        rounded={12}
+        justifyContent="center"
+        alignItems="center"
+        pv={4}
+        ph={8}
+        gap={4}
+      >
+        <SvgIcon name="spotify" size={16} />
+        {trackData && (
+          <Typo type="label-medium" numberOfLines={1}>
+            {trackData.artists[0].name} - {trackData.name}
+          </Typo>
+        )}
+      </Layout.FlexRow>
+    </StyledSpotifyMusic>
   );
 }
 export default SpotifyMusic;
