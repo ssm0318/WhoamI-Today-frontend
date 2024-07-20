1ca79baea37c026da0296b31e691da10bddc03ed
Gina <koyrkr@gmail.com>
2024-07-20 21:53:26 +0900

    feat: add profile nudge for noti icon

--- src/components/header/friends-header/FriendHeader.tsx
+++ src/components/header/friends-header/FriendHeader.tsx
@@ -1,13 +1,16 @@
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import Icon from '@components/_common/icon/Icon';
-import { SvgIcon } from '@design-system';
+import IconNudge from '@components/_common/icon-nudge/IconNudge';
+import { Layout, SvgIcon } from '@design-system';
+import { useBoundStore } from '@stores/useBoundStore';
 import { Noti } from '../Header.styled';
 import MainHeader from '../MainHeader';
 
 function FriendsHeader() {
   const [t] = useTranslation('translation', { keyPrefix: 'nav_tab' });
   const navigate = useNavigate();
+  const currentUser = useBoundStore((state) => state.myProfile);
 
   return (
     <MainHeader
@@ -17,8 +20,9 @@ function FriendsHeader() {
           <Icon name="add_user" size={44} onClick={() => navigate('/friends/explore')} />
           <Noti to="/notifications">
             <SvgIcon name="notification" size={44} />
-            {/* TODO 넛지 변경 */}
-            {/* {myProfile?.unread_noti && <IconNudge />} */}
+            <Layout.Absolute t={4} r={4}>
+              {currentUser?.unread_noti && <IconNudge />}
+            </Layout.Absolute>
           </Noti>
         </>
       }
