f8a3aac67dea790bdd888e83c373f312dd309894
Gina <koyrkr@gmail.com>
2024-07-27 12:56:50 +0900

    (#547) 탭 별 pull to refresh 구현 (#549)
    feat: add pull-to-refresh feature & applied in Friends & My page

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -1,15 +1,21 @@
+import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import Divider from '@components/_common/divider/Divider';
 import Icon from '@components/_common/icon/Icon';
+import PullToRefresh from '@components/_common/pull-to-refresh/PullToRefresh';
 import NoteSection from '@components/note/note-section/NoteSection';
 import Profile from '@components/profile/Profile';
 import ResponseSection from '@components/response/response-section/ResponseSection';
 import { Layout, Typo } from '@design-system';
 import { useBoundStore } from '@stores/useBoundStore';
+import { getMyNotes, getMyResponses } from '@utils/apis/my';
 
 function My() {
-  const { myProfile } = useBoundStore((state) => ({ myProfile: state.myProfile }));
+  const { myProfile, fetchCheckIn } = useBoundStore((state) => ({
+    myProfile: state.myProfile,
+    fetchCheckIn: state.fetchCheckIn,
+  }));
   const [t] = useTranslation('translation', {
     keyPrefix: 'my',
   });
@@ -19,45 +25,53 @@ function My() {
     return navigate('/notes/new');
   };
 
+  const handleRefresh = useCallback(async () => {
+    await getMyResponses(null);
+    await getMyNotes(null);
+    await fetchCheckIn();
+  }, [fetchCheckIn]);
+
   return (
-    <Layout.FlexCol w="100%" bgColor="LIGHT">
-      <Divider width={8} bgColor="LIGHT" />
-      <Layout.FlexRow
-        w="100%"
-        alignItems="center"
-        justifyContent="space-between"
-        p={12}
-        bgColor="WHITE"
-        rounded={8}
-      >
-        <Profile user={myProfile} />
-      </Layout.FlexRow>
-      <Divider width={8} bgColor="LIGHT" />
-      <Layout.FlexCol pv={12} pl={12} w="100%" bgColor="WHITE" rounded={8}>
-        <ResponseSection />
-      </Layout.FlexCol>
-      <Divider width={8} bgColor="LIGHT" />
-      <Layout.FlexRow ph={15} pv={10} bgColor="WHITE" w="100%" rounded="8px 8px 0px 0px">
+    <PullToRefresh onRefresh={handleRefresh}>
+      <Layout.FlexCol w="100%" bgColor="LIGHT">
+        <Divider width={8} bgColor="LIGHT" />
         <Layout.FlexRow
-          rounded={30}
-          alignItems="center"
           w="100%"
+          alignItems="center"
           justifyContent="space-between"
-          ph={20}
-          pv={10}
-          outline="LIGHT_GRAY"
-          onClick={handleClickNewNote}
+          p={12}
+          bgColor="WHITE"
+          rounded={8}
         >
-          <Typo type="body-medium" color="DARK_GRAY">
-            {t('whats_on_your_mind', { username: myProfile?.username })}
-          </Typo>
-          <Icon name="chat_media_image" size={24} fill="DARK_GRAY" />
+          <Profile user={myProfile} />
         </Layout.FlexRow>
-      </Layout.FlexRow>
-      <Layout.FlexCol pl={12} pb="default" w="100%" bgColor="WHITE" rounded="0px 0px 8px 8px">
-        <NoteSection />
+        <Divider width={8} bgColor="LIGHT" />
+        <Layout.FlexCol pv={12} pl={12} w="100%" bgColor="WHITE" rounded={8}>
+          <ResponseSection />
+        </Layout.FlexCol>
+        <Divider width={8} bgColor="LIGHT" />
+        <Layout.FlexRow ph={15} pv={10} bgColor="WHITE" w="100%" rounded="8px 8px 0px 0px">
+          <Layout.FlexRow
+            rounded={30}
+            alignItems="center"
+            w="100%"
+            justifyContent="space-between"
+            ph={20}
+            pv={10}
+            outline="LIGHT_GRAY"
+            onClick={handleClickNewNote}
+          >
+            <Typo type="body-medium" color="DARK_GRAY">
+              {t('whats_on_your_mind', { username: myProfile?.username })}
+            </Typo>
+            <Icon name="chat_media_image" size={24} fill="DARK_GRAY" />
+          </Layout.FlexRow>
+        </Layout.FlexRow>
+        <Layout.FlexCol pl={12} pb="default" w="100%" bgColor="WHITE" rounded="0px 0px 8px 8px">
+          <NoteSection />
+        </Layout.FlexCol>
       </Layout.FlexCol>
-    </Layout.FlexCol>
+    </PullToRefresh>
   );
 }
 
