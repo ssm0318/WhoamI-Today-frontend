41ad65af547c37a57dbb0e41ca640237d80d6c58
GooJinSun <gjinsun96@gmail.com>
2024-08-17 15:17:24 +0900

    bugfix: #597 #189 GooJinSun/WhoAmI-Today-backend#303 기존 프로필 이미지 캐시 버스팅 로직 삭제

--- src/components/_common/profile-image/ProfileImage.tsx
+++ src/components/_common/profile-image/ProfileImage.tsx
@@ -6,32 +6,15 @@ interface ProfileImageProps {
   size?: number;
   username?: string;
   className?: string;
-  /**
-   * 프로필 이미지 변경 시에도 동일한 이미지 이름의 소스를 사용하기 때문에 e.g {username}.png
-   * 강제로 이미지 업데이트가 필요한 경우 사용(e.g 설정>프로필 이미지 변경 이후 설정 페이지로 돌아온 경우)
-   */
-  ts?: number;
   onClick?: () => void;
 }
 
-function ProfileImage({
-  imageUrl,
-  username,
-  className,
-  ts,
-  size = 36,
-  onClick,
-}: ProfileImageProps) {
+function ProfileImage({ imageUrl, username, className, size = 36, onClick }: ProfileImageProps) {
   return (
     <NonShrinkWrapper onClick={onClick}>
       <Layout.LayoutBase w={size} h={size} rounded={size / 2} className={className}>
         {imageUrl ? (
-          <img
-            src={ts ? `${imageUrl}?ts=${ts}` : imageUrl}
-            width={size}
-            height={size}
-            alt={`${username ?? 'user'}-profile`}
-          />
+          <img src={imageUrl} width={size} height={size} alt={`${username ?? 'user'}-profile`} />
         ) : (
           <SvgIcon name="default_profile" size={size} />
         )}
