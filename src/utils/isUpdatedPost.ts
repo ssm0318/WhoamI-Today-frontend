9d61a924baaa88ab27df511cacec058e05fcddc0
GooJinSun <gjinsun96@gmail.com>
2024-08-24 20:38:20 +0900

    feat: #583 is_edited 필드로 수정

--- src/utils/isUpdatedPost.ts
+++
@@ -1,6 +0,0 @@
-import { ContentsCommon } from '@models/post';
-
-export const isUpdated = (
-  created_at: ContentsCommon['created_at'],
-  updated_at: ContentsCommon['updated_at'],
-) => created_at.split('.')[0] !== updated_at.split('.')[0];
