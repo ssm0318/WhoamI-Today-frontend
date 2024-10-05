59adf3f6599f03b16e42b9ad2c2fb0944f39a743
Gina <koyrkr@gmail.com>
2024-10-05 17:35:51 +0900

    fix: api url for likes in note & response

--- src/utils/apis/note.ts
+++ src/utils/apis/note.ts
@@ -89,7 +89,7 @@ export const readNote = async (ids: number[]) => {
 export const getNoteDetailLikes = async (noteId: number, page: string | null) => {
   const requestPage = page ? page.split('page=')[1] : null;
   const { data } = await axios.get<PaginationResponse<Like[]>>(
-    `/notes/${noteId}/likes/${!requestPage ? '' : `?page=${requestPage}`}/`,
+    `/notes/${noteId}/likes/${!requestPage ? '' : `?page=${requestPage}`}`,
   );
   return data;
 };
