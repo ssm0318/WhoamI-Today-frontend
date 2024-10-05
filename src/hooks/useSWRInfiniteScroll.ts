b0d27ed64bb38b0c4551ef1908db4c69029a9fe7
july001 <sk92983013@gmail.com>
2024-10-05 15:15:29 +0900

    feat: #656 useSWRInfinite와 useInfiniteScroll을 공통으로 적용할 수 있는 훅 생성 (useSWRInfiniteScroll)

---
+++ src/hooks/useSWRInfiniteScroll.ts
@@ -0,0 +1,46 @@
+import useSWRInfinite from 'swr/infinite';
+import useInfiniteScroll from '@hooks/useInfiniteScroll';
+import { PaginationResponse } from '@models/api/common';
+import axios from '@utils/apis/axios';
+
+const fetcher = async <T>(url: string) => {
+  const { data } = await axios.get<PaginationResponse<T[]>>(url);
+  return data;
+};
+
+interface Props {
+  key: string;
+}
+
+export function useSWRInfiniteScroll<T>({ key }: Props) {
+  const getKey = (pageIndex: number, previousPageData: PaginationResponse<T[]>) => {
+    if (previousPageData && !previousPageData.next) return null; // 끝에 도달
+    return `${key}?page=${pageIndex + 1}`; // SWR 키
+  };
+
+  const { isLoading, data, size, mutate, setSize } = useSWRInfinite(getKey, fetcher<T>);
+
+  const isEndPage = data && !data[size - 1]?.next;
+  const isLoadingMore = isLoading || (size > 0 && data && typeof data[size - 1] === 'undefined');
+
+  const fetchNextData = async () => {
+    if (isEndPage) return;
+    setSize((prevSize) => prevSize + 1);
+  };
+
+  const { targetRef, setIsLoading } = useInfiniteScroll<HTMLDivElement>(() => {
+    fetchNextData();
+    setIsLoading(false);
+  });
+
+  return {
+    targetRef,
+    isLoading,
+    isLoadingMore,
+    data,
+    size,
+    isEndPage,
+    mutate,
+    setSize,
+  };
+}
