f102c76beabf3acd8d8423c603d6c4483058fbc2
Gina <koyrkr@gmail.com>
2024-10-15 22:40:56 +0900

    feat: add EmojiButton for PostFooter

---
+++ src/components/_common/emoji-button/EmojiButton.tsx
@@ -0,0 +1,26 @@
+import { MouseEvent } from 'react';
+import { Layout } from '@design-system';
+import { Note, Response } from '@models/post';
+import Icon from '../icon/Icon';
+
+interface EmojiButtonProps {
+  post: Response | Note;
+  onClick: () => void;
+}
+
+function EmojiButton({ post, onClick }: EmojiButtonProps) {
+  const { id } = post;
+
+  const handleClick = (e: MouseEvent) => {
+    e.stopPropagation();
+    onClick();
+  };
+
+  return (
+    <Layout.FlexRow alignItems="center" m={0}>
+      {id && <Icon name="emoji" size={23} onClick={handleClick} />}
+    </Layout.FlexRow>
+  );
+}
+
+export default EmojiButton;
