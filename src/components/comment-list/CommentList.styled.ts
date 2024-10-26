b775b704ee021e4b5a27edb7f394a892c4b7ee8e
Gina <koyrkr@gmail.com>
2024-10-26 15:59:22 +0900

    fix: remove unnecessary styles

--- src/components/comment-list/CommentList.styled.ts
+++ src/components/comment-list/CommentList.styled.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
-import { MAX_WINDOW_WIDTH, Z_INDEX } from '@constants/layout';
+import { MAX_WINDOW_WIDTH } from '@constants/layout';
 import { Layout } from '@design-system';
 
 export const StyledCommentListFooter = styled(Layout.Fixed)`
   max-width: ${MAX_WINDOW_WIDTH}px;
-  z-index: ${Z_INDEX.COMMENT_LIST};
 `;
