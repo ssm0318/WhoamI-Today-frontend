cb5b701c2d9397fd6f4f6b715a0f4cf66e247dde
july001 <sk92983013@gmail.com>
2024-11-02 19:28:27 +0900

    refact: #684 콘솔로그 제거

--- src/utils/apis/translate.ts
+++ src/utils/apis/translate.ts
@@ -2,7 +2,5 @@ import { TranslateTextResponse } from '@models/api/translate';
 import axios from '@utils/apis/axios';
 
 export const requestTranslateText = async (text: string, target_language: string) => {
-  console.log({ text, target_language });
-
   return axios.post<TranslateTextResponse>('/translate/', { text, target_language });
 };
