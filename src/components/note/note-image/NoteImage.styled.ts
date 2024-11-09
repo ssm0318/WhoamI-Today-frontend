8e3ac440740f9f7b9b923192c26fcaacc600b48e
Jinsun Goo <37523788+GooJinSun@users.noreply.github.com>
2024-11-09 15:50:40 +0900

    (#561) 노트 이미지 개수를 1개로 수정 & 노트 이미지를 자유롭게 크롭 가능하도록 변경 (#704)
    * refact: #561 사용하지 않는 컴포넌트 제거
    
    * feat: #561 react-image-crop 추가
    
    * feat: #561 react-easy-crop 대신 자유 크롭이 가능한 react-iamge-crop 으로 노트 이미지 크롭 대체
    
    * feat: #561 노트 이미지를 한개로 변경 - <ImageSlider/> 관련 패키지, 컴포넌트 제거
    
    * style: #561 노트 디테일 페이지의 padding을 유저 페이지와 동일하게 변경
    
    * refact: #561 노트이미지 상수 코멘트 수정
    
    * refact: #561 styledComponent 적용해 중복 코드 제거
    
    * refact: #561 변수명 수정, 다국어 적용
    
    * refact: #561 remove test comments

--- src/components/note/note-image/NoteImage.styled.ts
+++ src/components/note/note-image/NoteImage.styled.ts
@@ -5,30 +5,16 @@ import {
   NOTE_IMAGE_DISPLAY_WIDTH,
   NOTE_IMAGE_DISPLAY_WIDTH_EDIT_MODE,
 } from '@constants/size';
-import { Layout } from '@design-system';
 
-export const ImageSliderWrapper = styled(Layout.FlexCol)`
-  touchaction: none;
-`;
-
-export const ImageWrapper = styled(Layout.FlexRow)`
-  position: relative;
-`;
-
-interface ImageProps {
+interface StyledNoteImageProps {
   isEditMode?: boolean;
 }
 
-export const Image = styled.img<ImageProps>`
+export const NoteImage = styled.img<StyledNoteImageProps>`
   width: ${({ isEditMode }) =>
     isEditMode ? NOTE_IMAGE_DISPLAY_WIDTH_EDIT_MODE : NOTE_IMAGE_DISPLAY_WIDTH}px;
   height: ${({ isEditMode }) =>
     isEditMode ? NOTE_IMAGE_DISPLAY_HEIGHT_EDIT_MODE : NOTE_IMAGE_DISPLAY_HEIGHT}px;
   object-fit: cover;
-`;
-
-export const IndicatorItem = styled(Layout.LayoutBase)<{
-  isActive: boolean;
-}>`
-  opacity: ${({ isActive }) => (isActive ? 1 : 0.5)};
+  border-radius: 17px;
 `;
--- src/components/note/note-item/NoteItem.tsx
+++ src/components/note/note-item/NoteItem.tsx
@@ -11,7 +11,7 @@ import { Layout, Typo } from '@design-system';
 import { Note, POST_DP_TYPE } from '@models/post';
 import { useBoundStore } from '@stores/useBoundStore';
 import { convertTimeDiffByString } from '@utils/timeHelpers';
-import NoteImageList from '../note-image-list/NoteImageList';
+import { NoteImage } from '../note-image/NoteImage.styled';
 
 interface NoteItemProps {
   note: Note;
@@ -123,8 +123,12 @@ function NoteItem({ note, isMyPage, displayType = 'LIST', refresh }: NoteItemPro
           <Typo type="body-large" color="BLACK" pre>
             {content}
           </Typo>
-          {/* 이미지 */}
-          <NoteImageList images={images} />
+          {/* 노트 이미지 - 1개만 노출 */}
+          {images[0] && (
+            <Layout.FlexRow w="100%" mv={10}>
+              <NoteImage src={images[0]} />
+            </Layout.FlexRow>
+          )}
           {/* (수정됨) */}
           {is_edited && (
             <Typo type="label-medium" color="MEDIUM_GRAY">
