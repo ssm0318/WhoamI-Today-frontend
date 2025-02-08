d9a791b23d5e703ef111ed8a175e0938ea7ca34e
july001 <sk92983013@gmail.com>
2025-02-08 13:30:50 +0900

    refact: #764 기능 플래그를 설명하는 주석 추가

--- src/constants/featureFlag.ts
+++ src/constants/featureFlag.ts
@@ -1,7 +1,9 @@
 import { VersionType } from '@models/api/user';
 
 export enum FeatureFlagKey {
+  /** 친구탭 페이지에서 친구 피드(친구들의 게시물을 최신순으로 노출)를 노출하는 플래그 */
   FRIEND_FEED = 'friendFeed',
+  /** 친구탭 페이지에서 전체 친구 목록을 노출하는 플래그 */
   FRIEND_LIST = 'friendList',
 }
 
