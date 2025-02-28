67ff97e24bd118caa2d2480db580580bf151dcfc
Yoojin Hong <dbwk18@kaist.ac.kr>
2025-02-28 17:51:50 +0900

    question tab branching

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -6,6 +6,7 @@ import { MAIN_SCROLL_CONTAINER_ID } from '@constants/scroll';
 import { Layout, SvgIcon, Typo } from '@design-system';
 import { resetScrollPosition } from '@hooks/useRestoreScrollPosition';
 import { useBoundStore } from '@stores/useBoundStore';
+import { UserSelector } from '@stores/user';
 import { NavTabItem, StyledTabItem, TabWrapper } from './Tab.styled';
 
 interface TabItemProps {
@@ -63,6 +64,8 @@ function TabItem({ to, type, size = 48 }: TabItemProps) {
 
 export default function Tab() {
   const location = useLocation();
+  const { featureFlags } = useBoundStore(UserSelector);
+
   const showFloatingButton =
     location.pathname === '/friends' ||
     location.pathname === '/my' ||
@@ -74,7 +77,7 @@ export default function Tab() {
         <TabItem to="/friends" type="friends" size={28} />
         <TabItem to="/my" type="my" size={28} />
         {/* <TabItem to="/chats" type="chats" size={28} /> */}
-        <TabItem to="/questions" type="questions" size={28} />
+        {featureFlags?.friendList && <TabItem to="/questions" type="questions" size={28} />}
       </Layout.FlexRow>
       {showFloatingButton && <FloatingButton />}
     </TabWrapper>
