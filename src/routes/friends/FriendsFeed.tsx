254c43366f0478898f815bbdfe01e03d37f6aef7
Yoojin Hong <dbwk18@kaist.ac.kr>
2025-03-07 18:37:38 +0900

    profile friend detail page branching

--- src/routes/friends/FriendsFeed.tsx
+++ src/routes/friends/FriendsFeed.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import Loader from '@components/_common/loader/Loader';
 import NoContents from '@components/_common/no-contents/NoContents';
@@ -11,7 +11,6 @@ import { useSWRInfiniteScroll } from '@hooks/useSWRInfiniteScroll';
 import { Note } from '@models/post';
 import { useBoundStore } from '@stores/useBoundStore';
 import { getAllFeed } from '@utils/apis/feed';
-import { getMyNotes } from '@utils/apis/my';
 import { MainScrollContainer } from 'src/routes/Root';
 
 function FriendsFeed() {
@@ -36,17 +35,8 @@ function FriendsFeed() {
     isLoadingMore: isNotesLoadingMore,
     mutate: refetchFeed,
   } = useSWRInfiniteScroll<Note>({
-    // key: `/user/me/notes/`,
     key: `/user/feed/`,
   });
-
-  useEffect(() => {
-    console.log(isLoading, notes);
-
-    const data = getMyNotes(null);
-    const data2 = getAllFeed(null);
-    console.log(data, data2);
-  }, [isLoading, notes]);
 
   return (
     <MainScrollContainer scrollRef={scrollRef}>
@@ -78,155 +68,5 @@ function FriendsFeed() {
     </MainScrollContainer>
   );
 }
-
-// function Friends() {
-//   const [t] = useTranslation('translation', { keyPrefix: 'friends' });
-
-//   const {
-//     targetRef,
-//     allFriends,
-//     isAllFriendsLoading,
-//     isLoadingMoreAllFriends,
-//     updateFriendList,
-//     refetchAllFriends,
-//   } = useInfiniteFetchFriends();
-
-//   const { favoriteFriends, refetchFavoriteFriends, isFavoriteFriendsLoading } =
-//     useFetchFavoriteFriends();
-
-//   const navigate = useNavigate();
-//   const handleClickEditFriends = () => {
-//     navigate('edit');
-//   };
-
-//   const handleRefresh = async () => {
-//     refetchFavoriteFriends();
-//     refetchAllFriends();
-//   };
-
-//   const handleClickExploreFriends = () => {
-//     navigate('/friends/explore');
-//   };
-
-//   const { scrollRef } = useRestoreScrollPosition('friendsPage');
-
-//   return (
-//     <MainScrollContainer scrollRef={scrollRef}>
-//       <PullToRefresh onRefresh={handleRefresh}>
-//         <Layout.FlexCol w="100%">
-//           {/* Favorites */}
-//           <Collapse
-//             title={[t('favorites'), !!favoriteFriends && `(${favoriteFriends.length})`]
-//               .filter(Boolean)
-//               .join(' ')}
-//             collapsedItem={
-//               <Layout.FlexRow
-//                 w="100%"
-//                 gap={10}
-//                 pv={12}
-//                 ph={8}
-//                 style={{ flexWrap: 'wrap', rowGap: '20px' }}
-//                 justifyContent="space-evenly"
-//               >
-//                 {isFavoriteFriendsLoading ? (
-//                   <FavoriteFriendListLoader />
-//                 ) : favoriteFriends && favoriteFriends.length > 0 ? (
-//                   favoriteFriends.map((user) => <FavoriteFriendItem key={user.id} user={user} />)
-//                 ) : (
-//                   <Layout.FlexCol alignItems="center" ph={75} gap={8}>
-//                     <Typo type="label-medium" color="DARK_GRAY">
-//                       {t('add_favorite')}
-//                     </Typo>
-//                     <Icon name="add_default" onClick={handleClickEditFriends} />
-//                   </Layout.FlexCol>
-//                 )}
-//               </Layout.FlexRow>
-//             }
-//           />
-//           <Divider width={1} marginLeading={9} />
-//           {/* All Friends */}
-//           <SwipeLayoutList>
-//             <Layout.FlexCol w="100%">
-//               {/* Friend Requests */}
-//               <Layout.FlexRow
-//                 w="100%"
-//                 ph={16}
-//                 pv={12}
-//                 justifyContent="space-between"
-//                 alignItems="center"
-//               >
-//                 <Typo type="title-medium">{t('all_friends')}</Typo>
-//                 <Button.Tertiary
-//                   status="normal"
-//                   text={t('edit_friends.title')}
-//                   onClick={handleClickEditFriends}
-//                   icon={<SvgIcon name="edit_filled" size={16} />}
-//                   iconPosition="left"
-//                   fontType="body-medium"
-//                 />
-//               </Layout.FlexRow>
-//               <Layout.FlexCol w="100%" pv={8} gap={4}>
-//                 {isAllFriendsLoading ? (
-//                   <AllFriendListLoader />
-//                 ) : allFriends?.[0] && allFriends[0].count > 0 ? (
-//                   <>
-//                     {/* Explore Friends */}
-//                     <Layout.FlexRow w="100%" ph={16} gap={16}>
-//                       <StyledUpdatedFriendItem
-//                         w="100%"
-//                         alignItems="center"
-//                         justifyContent="space-between"
-//                       >
-//                         <Layout.FlexRow
-//                           alignItems="center"
-//                           gap={7}
-//                           onClick={handleClickExploreFriends}
-//                         >
-//                           <Icon name="add_user" background="LIGHT_GRAY" size={44} />
-//                           <Layout.FlexCol>
-//                             <Layout.FlexRow gap={4} alignItems="center">
-//                               <Typo type="label-large">{t('explore_friends.title')}</Typo>
-//                             </Layout.FlexRow>
-//                           </Layout.FlexCol>
-//                         </Layout.FlexRow>
-//                       </StyledUpdatedFriendItem>
-//                     </Layout.FlexRow>
-//                     {/* 친구 목록 */}
-//                     {allFriends.map(({ results }) =>
-//                       results?.map((user) => {
-//                         if (user.is_hidden) return null;
-//                         return (
-//                           <UpdatedFriendItem
-//                             key={`friends_${user.id}`}
-//                             user={user}
-//                             updateFriendList={updateFriendList}
-//                             updateFavoriteFriendList={refetchFavoriteFriends}
-//                           />
-//                         );
-//                       }),
-//                     )}
-//                     <div ref={targetRef} />
-//                     {isLoadingMoreAllFriends && <AllFriendItemLoader />}
-//                   </>
-//                 ) : (
-//                   <Layout.FlexCol alignItems="center" ph={75} gap={8} w="100%">
-//                     <Typo type="label-medium" color="DARK_GRAY">
-//                       {t('no_friends')}
-//                     </Typo>
-//                     <Icon
-//                       name="add_user"
-//                       background="LIGHT_GRAY"
-//                       onClick={() => navigate('/friends/explore')}
-//                     />
-//                   </Layout.FlexCol>
-//                 )}
-//               </Layout.FlexCol>
-//             </Layout.FlexCol>
-//           </SwipeLayoutList>
-//         </Layout.FlexCol>
-//       </PullToRefresh>
-//     </MainScrollContainer>
-//   );
-// }
 
 export default FriendsFeed;
