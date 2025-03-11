f14b3e29e0401d1197fe296546b832df60123885
Gina Park <koyrkr@gmail.com>
2025-03-11 23:36:22 +0900

    feat: Add like count display for PostFooterDefault

--- src/components/_common/post-footer/PostFooterDefault.tsx
+++ src/components/_common/post-footer/PostFooterDefault.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { Layout, Typo } from '@design-system';
 import { Note, POST_DP_TYPE, POST_TYPE, Response } from '@models/post';
 import { User } from '@models/user';
+import { useBoundStore } from '@stores/useBoundStore';
+import { UserSelector } from '@stores/user';
 import Icon from '../icon/Icon';
 import LikeButton from '../like-button/LikeButton';
 import ProfileImageList from '../profile-image-list/ProfileImageList';
@@ -17,6 +19,9 @@ type PostFooterProps = {
   setInputFocus: () => void;
 };
 
+// default 버전
+// - 이모지 리액션 불가능
+// - 리액션 숫자 노출
 function PostFooterDefault({
   likedUserList,
   isMyPage,
@@ -25,9 +30,9 @@ function PostFooterDefault({
   showComments,
   setInputFocus,
 }: PostFooterProps) {
-  const { comment_count, type } = post;
+  const { comment_count, type, like_count } = post;
   const navigate = useNavigate();
-
+  const { featureFlags } = useBoundStore(UserSelector);
   const [t] = useTranslation('translation', {
     keyPrefix: post.type === POST_TYPE.RESPONSE ? 'responses' : 'notes',
   });
@@ -50,22 +55,22 @@ function PostFooterDefault({
 
   return (
     <Layout.FlexCol gap={8}>
-      <Layout.FlexRow gap={16} alignItems="center">
-        {likedUserList?.length > 0 && (
+      <Layout.FlexRow gap={8} alignItems="center">
+        {/* 좋아요 리스트 */}
+        {isMyPage && likedUserList?.length > 0 && (
           <button type="button" onClick={handleClickLikes}>
             <ProfileImageList images={likedUserList.map((user) => user.profile_image)} />
           </button>
         )}
         {!isMyPage && <LikeButton postType={type} post={post} iconSize={23} m={0} />}
-        {/* {isMyPage ? (
-          likedUserList?.length > 0 && (
-            <button type="button" onClick={handleClickLikes}>
-              <ProfileImageList images={likedUserList.map((user) => user.profile_image)} />
-            </button>
-          )
-        ) : (
-          <LikeButton postType={type} post={post} iconSize={23} m={0} />
-        )} */}
+        {/* 좋아요 수 */}
+        {featureFlags?.reactionCount && !!like_count && like_count > 0 && (
+          <button type="button" onClick={handleClickLikes}>
+            <Typo type="label-large" color="BLACK" underline>
+              {like_count} {t('likes')}
+            </Typo>
+          </button>
+        )}
         <Icon name="add_comment" size={23} onClick={handleClickCommentIcon} />
       </Layout.FlexRow>
       {!!comment_count && displayType === 'LIST' && (
