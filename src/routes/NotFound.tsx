2d39fede9fae9f923cd488491543894ff5b4bf5b
july001 <sk92983013@gmail.com>
2025-03-15 17:46:10 +0900

    feat: #768 메인 페이지 이동 버튼 추가

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import SubHeader from '@components/sub-header/SubHeader';
 import { BOTTOM_TABBAR_HEIGHT } from '@constants/layout';
-import { Font, Layout } from '@design-system';
+import { Button, Font, Layout } from '@design-system';
 
 export function NotFound() {
-  const [t] = useTranslation('translation', { keyPrefix: 'error' });
+  const [t] = useTranslation('translation', { keyPrefix: 'not_found_page' });
   return (
     <>
       <SubHeader />
@@ -14,11 +14,12 @@ export function NotFound() {
         alignItems="center"
         justifyContent="center"
         mb={BOTTOM_TABBAR_HEIGHT}
+        gap={10}
       >
         <Font.Typo type="title-large" color="DARK_GRAY">
-          {t('not_found')}
+          {t('title')}
         </Font.Typo>
-        {/** 홈으로 이동 */}
+        <Button.Tertiary to="/" text={t('go_home')} status="normal" />
       </Layout.FlexCol>
     </>
   );
