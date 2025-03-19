ee76a63855883c1f05239824316e752623d5865a
Gina Park <koyrkr@gmail.com>
2025-03-19 16:31:41 +0900

    feat: Integrate i18n translations for check-in components, updating text to use localized strings

--- src/components/check-in/add-new-check-in/AddNewCheckIn.tsx
+++ src/components/check-in/add-new-check-in/AddNewCheckIn.tsx
@@ -1,7 +1,9 @@
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { Font, Layout, SvgIcon } from '@design-system';
 
 function AddNewCheckIn() {
+  const [t] = useTranslation('translation', { keyPrefix: 'check_in' });
   const navigate = useNavigate();
 
   const handleClickAdd = () => {
@@ -11,7 +13,7 @@ function AddNewCheckIn() {
   return (
     <Layout.FlexRow gap={8} w="100%" alignItems="center" justifyContent="center">
       <SvgIcon name="add_default" size={24} onClick={handleClickAdd} />
-      <Font.Body type="12_regular">New Check-in</Font.Body>
+      <Font.Body type="12_regular">{t('new_check_in')}</Font.Body>
     </Layout.FlexRow>
   );
 }
