314578969507efb43ed7903c98fc8b7bcaee4b16
Gina <koyrkr@gmail.com>
2025-03-20 21:28:42 +0900

    (#890) 스크롤 이상 (#944)
    refact: Replace MainContainer with MainScrollContainer in UserPage for improved layout handling and conditional rendering of user data

--- src/routes/UserPage.tsx
+++ src/routes/UserPage.tsx
@@ -2,19 +2,18 @@ import { useContext, useEffect, useState } from 'react';
 import { Outlet, useNavigate, useOutlet, useParams } from 'react-router-dom';
 import CommonError from '@components/_common/common-error/CommonError';
 import { Divider } from '@components/_common/divider/Divider.styled';
-import MainContainer from '@components/_common/main-container/MainContainer';
 import UserHeader from '@components/header/user-header/UserHeader';
 import NoteSection from '@components/note/note-section/NoteSection';
 import Profile from '@components/profile/Profile';
 import ResponseSection from '@components/response/response-section/ResponseSection';
 import UserMoreModal from '@components/user-page/UserMoreModal';
 import { UserPageContext } from '@components/user-page/UserPage.context';
-import { TITLE_HEADER_HEIGHT } from '@constants/layout';
 import { Layout } from '@design-system';
 import useAsyncEffect from '@hooks/useAsyncEffect';
 import { useBoundStore } from '@stores/useBoundStore';
 import { UserSelector } from '@stores/user';
 import { readFriendCheckIn } from '@utils/apis/checkIn';
+import { MainScrollContainer } from './Root';
 
 function UserPage() {
   const { username } = useParams();
@@ -47,49 +46,51 @@ function UserPage() {
 
   return (
     <>
-      <MainContainer key={username} style={{ display: outlet ? 'none' : 'block' }}>
-        <UserHeader username={username} onClickMore={handleClickMore} userId={userId} />
-        <Layout.FlexCol w="100%" bgColor="LIGHT" mt={TITLE_HEADER_HEIGHT}>
-          {user.state === 'hasError' && <CommonError />}
-          {user.state === 'hasValue' && user.data && (
-            <UserMoreModal
-              isVisible={showMore}
-              setIsVisible={setShowMore}
-              user={user.data}
-              callback={updateUser}
-            />
-          )}
-          {user.state !== 'hasError' && (
-            <>
-              {/** profile section */}
-              <Layout.FlexRow
-                w="100%"
-                alignItems="center"
-                justifyContent="space-between"
-                p={12}
-                bgColor="WHITE"
-                rounded={8}
-              >
-                <Profile user={user.data} />
-              </Layout.FlexRow>
-              {featureFlags?.friendList && (
-                <>
-                  {/** responses and notes section */}
-                  <Divider width={8} bgColor="LIGHT" />
-                  <Layout.FlexCol pv={12} pl={12} w="100%" bgColor="WHITE" rounded={8}>
-                    <ResponseSection username={username} />
-                  </Layout.FlexCol>
-                </>
-              )}
+      {!outlet && (
+        <MainScrollContainer key={username}>
+          <UserHeader username={username} onClickMore={handleClickMore} userId={userId} />
+          <Layout.FlexCol w="100%" bgColor="LIGHT">
+            {user.state === 'hasError' && <CommonError />}
+            {user.state === 'hasValue' && user.data && (
+              <UserMoreModal
+                isVisible={showMore}
+                setIsVisible={setShowMore}
+                user={user.data}
+                callback={updateUser}
+              />
+            )}
+            {(user.state === 'loading' || user.state === 'hasValue') && (
+              <>
+                {/** profile section */}
+                <Layout.FlexRow
+                  w="100%"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  p={12}
+                  bgColor="WHITE"
+                  rounded={8}
+                >
+                  <Profile user={user.data} />
+                </Layout.FlexRow>
+                {featureFlags?.friendList && (
+                  <>
+                    {/** responses and notes section */}
+                    <Divider width={8} bgColor="LIGHT" />
+                    <Layout.FlexCol pv={12} pl={12} w="100%" bgColor="WHITE" rounded={8}>
+                      <ResponseSection username={username} />
+                    </Layout.FlexCol>
+                  </>
+                )}
 
-              <Divider width={8} bgColor="LIGHT" />
-              <Layout.FlexCol pt={12} pl={12} pb="default" w="100%" bgColor="WHITE" rounded={8}>
-                <NoteSection username={username} />
-              </Layout.FlexCol>
-            </>
-          )}
-        </Layout.FlexCol>
-      </MainContainer>
+                <Divider width={8} bgColor="LIGHT" />
+                <Layout.FlexCol pt={12} pl={12} pb="default" w="100%" bgColor="WHITE" rounded={8}>
+                  <NoteSection username={username} />
+                </Layout.FlexCol>
+              </>
+            )}
+          </Layout.FlexCol>
+        </MainScrollContainer>
+      )}
       <Outlet />
     </>
   );
