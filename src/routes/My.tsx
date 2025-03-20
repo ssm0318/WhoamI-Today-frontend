bbedb83d6c36f264e5d2fd3b7b2053e0256fef1a
Gina Park <koyrkr@gmail.com>
2025-03-20 23:33:51 +0900

    feat: Enhance NoteSection and ResponseSection components with new layout and navigation features. Added a clickable area for creating new notes in NoteSection and improved the layout in ResponseSection by incorporating a logo alongside the title.

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -1,14 +1,11 @@
 import { useCallback } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 import Divider from '@components/_common/divider/Divider';
-import Icon from '@components/_common/icon/Icon';
 import PullToRefresh from '@components/_common/pull-to-refresh/PullToRefresh';
 import { FLOATING_BUTTON_SIZE } from '@components/header/floating-button/FloatingButton.styled';
 import NoteSection from '@components/note/note-section/NoteSection';
 import Profile from '@components/profile/Profile';
 import ResponseSection from '@components/response/response-section/ResponseSection';
-import { Layout, Typo } from '@design-system';
+import { Layout } from '@design-system';
 import { useRestoreScrollPosition } from '@hooks/useRestoreScrollPosition';
 import { useBoundStore } from '@stores/useBoundStore';
 import { UserSelector } from '@stores/user';
@@ -20,15 +17,8 @@ function My() {
     myProfile: state.myProfile,
     fetchCheckIn: state.fetchCheckIn,
   }));
-  const [t] = useTranslation('translation', {
-    keyPrefix: 'my',
-  });
-  const navigate = useNavigate();
-  const { featureFlags } = useBoundStore(UserSelector);
 
-  const handleClickNewNote = () => {
-    return navigate('/notes/new');
-  };
+  const { featureFlags } = useBoundStore(UserSelector);
 
   const handleRefresh = useCallback(async () => {
     await Promise.all([getMyResponses(null), getMyNotes(null), fetchCheckIn()]);
@@ -60,24 +50,8 @@ function My() {
               <Divider width={8} bgColor="LIGHT" />
             </>
           )}
-          <Layout.FlexRow ph={15} pv={10} bgColor="WHITE" w="100%" rounded="8px 8px 0px 0px">
-            <Layout.FlexRow
-              rounded={30}
-              alignItems="center"
-              w="100%"
-              justifyContent="space-between"
-              ph={20}
-              pv={10}
-              outline="LIGHT_GRAY"
-              onClick={handleClickNewNote}
-            >
-              <Typo type="body-medium" color="DARK_GRAY">
-                {t('whats_on_your_mind', { username: myProfile?.username })}
-              </Typo>
-              <Icon name="chat_media_image" size={24} fill="DARK_GRAY" />
-            </Layout.FlexRow>
-          </Layout.FlexRow>
-          <Layout.FlexCol pl={12} pb="default" w="100%" bgColor="WHITE" rounded="0px 0px 8px 8px">
+
+          <Layout.FlexCol ph={12} pb="default" w="100%" bgColor="WHITE" rounded="0px 0px 8px 8px">
             <NoteSection />
           </Layout.FlexCol>
         </Layout.FlexCol>
