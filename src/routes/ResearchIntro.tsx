6ff3fea0e3900ad661aa9a4181a2eada5a1bed34
Gina Park <koyrkr@gmail.com>
2025-03-25 13:24:22 +0900

    feat: Add inquiry links for KakaoTalk, Discord, and Instagram in ResearchIntro component with appropriate handling for React Native WebView

--- src/routes/ResearchIntro.tsx
+++ src/routes/ResearchIntro.tsx
@@ -3,16 +3,20 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import MainContainer from '@components/_common/main-container/MainContainer';
 import {
+  RESEARCH_INQUIRY_DISCORD_LINK,
+  RESEARCH_INQUIRY_INSTAGRAM_LINK,
   RESEARCH_INQUIRY_KAKAOTALK_LINK,
   RESEARCH_SIGNUP_FORM_URL_EN,
   RESEARCH_SIGNUP_FORM_URL_KO,
 } from '@constants/url';
 import { Button, Layout, SvgIcon, Typo } from '@design-system';
+import { usePostAppMessage } from '@hooks/useAppMessage';
 
 function ResearchIntro() {
   const [t, i18n] = useTranslation('translation', { keyPrefix: 'research_intro' });
   const navigate = useNavigate();
   const [showMore, setShowMore] = useState(false);
+  const postMessage = usePostAppMessage();
 
   const handleClickSignUpForm = () => {
     window.open(
@@ -23,6 +27,44 @@ function ResearchIntro() {
 
   const handleClickSignIn = () => {
     navigate('/signin');
+  };
+
+  const handleClickKakaoInquiry = () => {
+    if (window.ReactNativeWebView) {
+      postMessage('OPEN_BROWSER', {
+        url: RESEARCH_INQUIRY_KAKAOTALK_LINK,
+      });
+    } else {
+      window.open(RESEARCH_INQUIRY_KAKAOTALK_LINK, '_blank');
+    }
+  };
+
+  const handleClickDiscordLink = () => {
+    if (window.ReactNativeWebView) {
+      postMessage('OPEN_BROWSER', {
+        url: RESEARCH_INQUIRY_DISCORD_LINK,
+      });
+    } else {
+      window.open(RESEARCH_INQUIRY_DISCORD_LINK, '_blank');
+    }
+  };
+
+  const handleClickInstagramLink = () => {
+    if (window.ReactNativeWebView) {
+      postMessage('OPEN_BROWSER', {
+        url: RESEARCH_INQUIRY_INSTAGRAM_LINK,
+      });
+    } else {
+      window.open(RESEARCH_INQUIRY_INSTAGRAM_LINK, '_blank');
+    }
+  };
+
+  const handleClickTextMessageInquiry = () => {
+    if (window.ReactNativeWebView) {
+      postMessage('OPEN_BROWSER', {
+        url: 'sms:+1-206-730-2178',
+      });
+    }
   };
 
   return (
@@ -64,34 +106,152 @@ function ResearchIntro() {
           <Layout.FlexCol w="100%" ph={20} mt={10}>
             <Typo type="body-medium">{t('someting_else_description')}</Typo>
             <Layout.FlexCol gap={12} mt={20}>
-              <Layout.FlexRow alignItems="center" gap={8}>
-                <span>📞</span>
-                <Typo type="body-medium">
-                  <a href="tel:+1-206-730-2178" target="_blank" rel="noopener noreferrer">
-                    +1-206-730-2178 🇺🇸
-                  </a>
-                </Typo>
-              </Layout.FlexRow>
-              <Layout.FlexRow alignItems="center" gap={8}>
-                <span>💬</span>
-                <Typo type="body-medium">
+              {i18n.language !== 'ko-KR' ? (
+                <Layout.FlexCol gap={15} pl={4}>
                   <a
                     href={RESEARCH_INQUIRY_KAKAOTALK_LINK}
                     target="_blank"
                     rel="noopener noreferrer"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleClickKakaoInquiry();
+                    }}
                   >
-                    [Kakaotalk link] 🇰🇷
+                    <Typo type="body-medium">
+                      💬{' '}
+                      <Typo type="body-medium" bold underline>
+                        {t('kakao')}
+                      </Typo>
+                    </Typo>
                   </a>
-                </Typo>
-              </Layout.FlexRow>
-              <Layout.FlexRow alignItems="center" gap={8}>
-                <span>📧</span>
-                <Typo type="body-medium">
-                  <a href="mailto:jaewonk@uw.edu" target="_blank" rel="noopener noreferrer">
-                    jaewonk@uw.edu
+                  <Typo type="body-medium">
+                    🎮{' '}
+                    <Typo type="body-medium" bold>
+                      {t('discord')} :{' '}
+                    </Typo>
+                    <a
+                      href={RESEARCH_INQUIRY_DISCORD_LINK}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleClickDiscordLink();
+                      }}
+                    >
+                      <Typo type="body-medium" underline>
+                        @jaewonkim___
+                      </Typo>
+                    </a>
+                    <Typo type="body-medium"> (밑줄 3개)</Typo>
+                  </Typo>
+                  <a
+                    href={RESEARCH_INQUIRY_INSTAGRAM_LINK}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleClickInstagramLink();
+                    }}
+                  >
+                    <Typo type="body-medium">
+                      📸{' '}
+                      <Typo type="body-medium" bold>
+                        {t('instagram')} :{' '}
+                      </Typo>
+                      <Typo type="body-medium" underline>
+                        @whoami.today.official
+                      </Typo>
+                    </Typo>
                   </a>
-                </Typo>
-              </Layout.FlexRow>
+                  <Layout.FlexRow alignItems="center" gap={8}>
+                    📧
+                    <Typo type="body-medium">
+                      <Typo type="body-medium" bold>
+                        {t('email')} :{' '}
+                      </Typo>
+                      <a href="mailto:jaewonk@uw.edu" target="_blank" rel="noopener noreferrer">
+                        jaewonk@uw.edu
+                      </a>
+                    </Typo>
+                  </Layout.FlexRow>
+                </Layout.FlexCol>
+              ) : (
+                <Layout.FlexCol gap={15} pl={4}>
+                  <Typo type="body-medium">
+                    🎮{' '}
+                    <Typo type="body-medium" bold>
+                      {t('discord')} :{' '}
+                    </Typo>
+                    <a
+                      href={RESEARCH_INQUIRY_DISCORD_LINK}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      onClick={(e) => {
+                        if (window.ReactNativeWebView) {
+                          e.preventDefault();
+                          handleClickDiscordLink();
+                        }
+                      }}
+                    >
+                      <Typo type="body-medium" underline>
+                        @jaewonkim___
+                      </Typo>
+                    </a>
+                    <Typo type="body-medium"> (three underscores)</Typo>
+                  </Typo>
+                  <a
+                    href="sms:+1-206-730-2178"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => {
+                      if (window.ReactNativeWebView) {
+                        e.preventDefault();
+                        handleClickTextMessageInquiry();
+                      }
+                    }}
+                  >
+                    <Typo type="body-medium">
+                      📱{' '}
+                      <Typo type="body-medium" bold>
+                        {t('text_message')} :{' '}
+                      </Typo>
+                      <Typo type="body-medium" underline>
+                        +1-206-730-2178
+                      </Typo>
+                    </Typo>
+                  </a>
+                  <a
+                    href={RESEARCH_INQUIRY_INSTAGRAM_LINK}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleClickInstagramLink();
+                    }}
+                  >
+                    <Typo type="body-medium">
+                      📸{' '}
+                      <Typo type="body-medium" bold>
+                        {t('instagram')} :{' '}
+                      </Typo>
+                      <Typo type="body-medium" underline>
+                        @whoami.today.official
+                      </Typo>
+                    </Typo>
+                  </a>
+                  <Layout.FlexRow alignItems="center" gap={8}>
+                    <span>📧</span>
+                    <Typo type="body-medium">
+                      <Typo type="body-medium" bold>
+                        {t('email')} :{' '}
+                      </Typo>
+                      <a href="mailto:jaewonk@uw.edu" target="_blank" rel="noopener noreferrer">
+                        jaewonk@uw.edu
+                      </a>
+                    </Typo>
+                  </Layout.FlexRow>
+                </Layout.FlexCol>
+              )}
             </Layout.FlexCol>
           </Layout.FlexCol>
         )}
