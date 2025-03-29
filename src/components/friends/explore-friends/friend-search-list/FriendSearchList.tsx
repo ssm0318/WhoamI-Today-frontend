89a857e65b99d98c24ae3113ae6c94b18a0d6a6c
Gina Park <koyrkr@gmail.com>
2025-03-29 15:46:13 +0900

    remove showEmail prop from FriendSearchList component for cleaner UI

--- src/components/friends/explore-friends/friend-search-list/FriendSearchList.tsx
+++ src/components/friends/explore-friends/friend-search-list/FriendSearchList.tsx
@@ -111,7 +111,6 @@ export default function FriendSearchList({ query }: Props) {
                   type="search"
                   user={user}
                   onClickUnfriend={handleClickUnfriend(user.id)}
-                  showEmail
                 />
               ))}
             </Layout.FlexCol>
@@ -130,7 +129,6 @@ export default function FriendSearchList({ query }: Props) {
                   user={user}
                   onClickRequest={handleClickRequest(user.id)}
                   onClickCancelRequest={handleClickCancelRequest(user.id)}
-                  showEmail
                 />
               ))}
             </Layout.FlexCol>
