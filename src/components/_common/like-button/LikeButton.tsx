d66c6ed4bab76411da7685f90e537779990b9b16
Gina Park <koyrkr@gmail.com>
2025-04-17 06:54:03 +0900

    feat: enhance LikeButton component with loading state and error handling for like/unlike actions

--- src/components/_common/like-button/LikeButton.tsx
+++ src/components/_common/like-button/LikeButton.tsx
@@ -1,4 +1,5 @@
 import { MouseEvent, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Layout, SvgIcon } from '@design-system';
 import {
   Comment,
@@ -8,6 +9,7 @@ import {
   QuestionResponse,
   Response,
 } from '@models/post';
+import { useBoundStore } from '@stores/useBoundStore';
 import { deleteLike, postLike } from '@utils/apis/likes';
 import * as S from './LikeButton.styled';
 
@@ -21,27 +23,50 @@ interface LikeButtonProps {
 
 function LikeButton({ postType, post, iconSize, m = 6, refresh }: LikeButtonProps) {
   const { id, current_user_like_id } = post;
+  const [t] = useTranslation('translation');
+  const { openToast } = useBoundStore((state) => ({
+    openToast: state.openToast,
+  }));
 
   const [likeId, setLikeId] = useState<number | null>(current_user_like_id);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     setLikeId(current_user_like_id);
   }, [current_user_like_id]);
 
-  const like = () => {
-    if (!id) return;
-    postLike({ target_id: id, target_type: postType }).then(({ id: like_id }) => {
+  const like = async () => {
+    if (!id || isLoading) return;
+
+    try {
+      setIsLoading(true);
+      const { id: like_id } = await postLike({ target_id: id, target_type: postType });
       setLikeId(like_id);
       refresh?.();
-    });
+    } catch (error) {
+      openToast({
+        message: t('error.like_duplicate'),
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  const unLike = () => {
-    if (!likeId) return;
-    deleteLike(likeId).then(() => {
+  const unLike = async () => {
+    if (!likeId || isLoading) return;
+
+    try {
+      setIsLoading(true);
+      await deleteLike(likeId);
       setLikeId(null);
       refresh?.();
-    });
+    } catch (error) {
+      openToast({
+        message: t('error.unlike_duplicate'),
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const toggleLike = (e: MouseEvent) => {
@@ -53,7 +78,7 @@ function LikeButton({ postType, post, iconSize, m = 6, refresh }: LikeButtonProp
   return (
     <Layout.FlexRow alignItems="center">
       {id && (
-        <S.IconButton type="button" m={m} onClick={toggleLike} size={iconSize}>
+        <S.IconButton type="button" m={m} onClick={toggleLike} size={iconSize} disabled={isLoading}>
           <SvgIcon name={likeId ? 'like_filled' : 'like'} size={iconSize} />
         </S.IconButton>
       )}
