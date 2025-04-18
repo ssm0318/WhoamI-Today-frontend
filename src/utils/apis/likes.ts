a96e840d7803ca2cad767e7d05cfac716af71f0c
Gina <koyrkr@gmail.com>
2025-04-18 10:18:07 +0900

    (#1108) 좋아요 중복 에러 메시지 노출  (#1110)
    * feat: enhance LikeButton component with loading state and error handling for like/unlike actions
    
    * feat: add duplicate like/unlike error messages to English and Korean translations
    
    * feat: improve error handling in LikeButton component by utilizing onError callbacks for like/unlike actions

--- src/utils/apis/likes.ts
+++ src/utils/apis/likes.ts
@@ -1,3 +1,4 @@
+import i18n from '@i18n/index';
 import { CommonTarget } from '@models/api/common';
 import axios from './axios';
 
@@ -9,11 +10,24 @@ interface PostLikeResponse extends CommonTarget {
   is_anonymous: boolean;
 }
 
-export const postLike = async (targetInfo: CommonTarget) => {
-  const { data } = await axios.post<PostLikeResponse>(`/likes/`, targetInfo);
+export const postLike = async (targetInfo: CommonTarget, onError: (errorMsg: string) => void) => {
+  const { data } = await axios.post<PostLikeResponse>(`/likes/`, targetInfo).catch((e) => {
+    if (e.response?.data.detail) {
+      onError(e.response.data.detail);
+    } else {
+      onError(i18n.t('error.temporary_error'));
+    }
+    throw e;
+  });
   return data;
 };
 
-export const deleteLike = async (likeId: number) => {
-  return axios.delete(`/likes/${likeId}/`);
+export const deleteLike = async (likeId: number, onError: (errorMsg: string) => void) => {
+  return axios.delete(`/likes/${likeId}/`).catch((e) => {
+    if (e.response?.data.detail) {
+      onError(e.response.data.detail);
+    } else {
+      onError(i18n.t('error.temporary_error'));
+    }
+  });
 };
