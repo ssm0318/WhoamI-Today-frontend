d24939d401ebe987c45864e958a26a52731fa903
Gina Park <koyrkr@gmail.com>
2025-05-02 13:38:06 +0900

    fix: update daily survey URLs for English and Korean versions to new Qualtrics links

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -35,5 +35,5 @@ export const USER_MANUAL_URL =
 export const ONBOARDING_VIDEO_URL =
   'https://www.youtube.com/playlist?list=PLeg7yLjhFvgw7tWm9QpU536mfHjLQWy7S';
 
-export const DAILY_SURVEY_URL_EN = 'https://uwashington.qualtrics.com/jfe/form/SV_1Gjnnq3QYN8y1h4';
-export const DAILY_SURVEY_URL_KO = 'https://uwashington.qualtrics.com/jfe/form/SV_6PWRLamoT9XqFdI';
+export const DAILY_SURVEY_URL_EN = 'https://uwashington.qualtrics.com/jfe/form/SV_b4017RNxNe5wvMq';
+export const DAILY_SURVEY_URL_KO = 'https://uwashington.qualtrics.com/jfe/form/SV_cCnY8BhEGcTrdrM';
