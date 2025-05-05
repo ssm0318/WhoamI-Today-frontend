34e138f79ad396149b5e2ae5f3fdfd4727d767b0
Gina Park <koyrkr@gmail.com>
2025-05-05 16:07:18 +0900

    feat: add logo to SuggestQuestions component for enhanced branding

--- src/routes/SuggestQuestions.tsx
+++ src/routes/SuggestQuestions.tsx
@@ -10,6 +10,14 @@ function SuggestQuestions() {
       <Layout.FlexCol mt={50} w="100%" h="100%" ph={25} alignItems="center">
         <Typo type="head-line" mb={50} textAlign="center">
           {t('title')}
+          <img
+            src="/whoami-logo.svg"
+            width="14px"
+            alt="who_am_i"
+            style={{
+              marginLeft: 8,
+            }}
+          />
         </Typo>
         <Typo type="title-medium" pre>
           {t('description')}
