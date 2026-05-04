dc5619f2205030ba68e9a58f5a5296861453e5a9
Gina Park <koyrkr@gmail.com>
2026-05-04 14:04:22 +0900

    Fix swipe action button height

--- src/components/_common/swipe-layout/SwipeButton.styled.ts
+++ src/components/_common/swipe-layout/SwipeButton.styled.ts
@@ -7,6 +7,7 @@ export const StyledSwipeButton = styled.button<{ backgroundColor?: keyof typeof 
   justify-content: center;
   width: 65px;
   height: 100%;
+  min-height: 44px;
   border: none;
   cursor: pointer;
   background-color: ${({ backgroundColor, theme }) =>
