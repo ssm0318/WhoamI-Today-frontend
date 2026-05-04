1e249d5d5ecfbf242879367b37da132f23c957a5
Gina <koyrkr@gmail.com>
2026-05-04 17:12:30 +0900

    Fix Android profile input focus (#1347)
    * Fix unchanged username profile saves
    
    * Fix Android profile inputs and local dev routing
    
    * Remove local dev routing changes from PR

--- src/design-system/Inputs/TextArea.styled.ts
+++ src/design-system/Inputs/TextArea.styled.ts
@@ -10,10 +10,17 @@ export const CommonTextArea = styled(TextareaAutosize).withConfig({
   shouldForwardProp: ((prop: string | number) =>
     String(prop) !== 'underline' && String(prop) !== 'width') as any,
 })`
+  -webkit-appearance: none;
+  appearance: none;
   ${(props: CommonTextAreaProps) => `width: ${props.width ? `${props.width}px` : '100%'};`}
   padding: 14px 0;
   font-size: 18px;
+  background-color: transparent;
   border: none;
+  border-radius: 0;
+  box-shadow: none;
+  caret-color: ${({ theme }) => theme.BLACK};
+  -webkit-tap-highlight-color: transparent;
   ${({ underline, theme }) =>
     underline &&
     `border-bottom-width: 1px;
